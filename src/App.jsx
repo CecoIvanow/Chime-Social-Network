2f3da82babeb21940518c83b87ba21816061b3f2
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-02-23 14:40:21 +0200

    Added user home page with friends section

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import MenuBar from './components/MenuBar.jsx'
+import UserHomePage from './pages/UserHomePage.jsx'
+
+export default function App() {
+    return <>
+        <MenuBar />
+        <UserHomePage />
+    </>
+}
