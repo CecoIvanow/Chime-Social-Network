9e3dfa839f33c28bcd2f3c1a6c5ae97c125da541
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-02-28 00:51:24 +0200

    Created the posts section

--- src/pages/UserHomePage.jsx
+++ src/pages/UserHomePage.jsx
@@ -4,11 +4,126 @@ export default function UserHomePage() {
     return <>
         <div className='user-home-page'>
             <div className="profile-section">
-            <p>fddff</p>
-            </div>
-            <div className="posts-section">
                 <p>fddff</p>
             </div>
+
+            <div className="posts-section">
+                <div className='post-create'>
+                    <form >
+                        <div className='post-header'>
+                            <img src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg" />
+                            <label htmlFor="post"></label>
+                            <input type="text" name="post" id="post" />
+                        </div>
+                        <button className='submit-button' >Post</button>
+                    </form>
+                </div>
+                <div className='posts-list'>
+                    <ul>
+                        <li className='post-item'>
+                            <div className='post-header'>
+                                <img className='owner-picture' src="https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww" alt="" />
+                                <div className='post-owner'>John Doe</div>
+                            </div>
+                            <div className='post-text'>Just climbed Mount Everest! 🏔️ What an incredible experience!</div>
+                            <div className='post-buttons-div'>
+                                <div>
+                                    <button className='post-buttons like-btn' type="button">Like</button>
+                                </div>
+                                <div className='owner-buttons'>
+                                    <button className='post-buttons edit-btn' type="button">Edit</button>
+                                    <button className='post-buttons delete-btn' type="button">Delete</button>
+                                </div>
+                            </div>
+                        </li>
+                        <li className='post-item'>
+                            <div className='post-header'>
+                                <img className='owner-picture' src="https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww" alt="" />
+                                <div className='post-owner'>John Doe</div>
+                            </div>
+                            <div className='post-text'>Just climbed Mount Everest! 🏔️ What an incredible experience!</div>
+                            <div className='post-buttons-div'>
+                                <button className='post-buttons like-btn' type="button">Like</button>
+                                <div className='owner-buttons'>
+                                    <button className='post-buttons edit-btn' type="button">Edit</button>
+                                    <button className='post-buttons delete-btn' type="button">Delete</button>
+                                </div>
+                            </div>
+                        </li>
+                        <li className='post-item'>
+                            <div className='post-header'>
+                                <img className='owner-picture' src="https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww" alt="" />
+                                <div className='post-owner'>John Doe</div>
+                            </div>
+                            <div className='post-text'>Just climbed Mount Everest! 🏔️ What an incredible experience!</div>
+                            <div className='post-buttons-div'>
+                                <button className='post-buttons like-btn' type="button">Like</button>
+                                <div className='owner-buttons'>
+                                    <button className='post-buttons edit-btn' type="button">Edit</button>
+                                    <button className='post-buttons delete-btn' type="button">Delete</button>
+                                </div>
+                            </div>
+                        </li>
+                        <li className='post-item'>
+                            <div className='post-header'>
+                                <img className='owner-picture' src="https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww" alt="" />
+                                <div className='post-owner'>John Doe</div>
+                            </div>
+                            <div className='post-text'>Just climbed Mount Everest! 🏔️ What an incredible experience!</div>
+                            <div className='post-buttons-div'>
+                                <button className='post-buttons like-btn' type="button">Like</button>
+                                <div className='owner-buttons'>
+                                    <button className='post-buttons edit-btn' type="button">Edit</button>
+                                    <button className='post-buttons delete-btn' type="button">Delete</button>
+                                </div>
+                            </div>
+                        </li>
+                        <li className='post-item'>
+                            <div className='post-header'>
+                                <img className='owner-picture' src="https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww" alt="" />
+                                <div className='post-owner'>John Doe</div>
+                            </div>
+                            <div className='post-text'>Just climbed Mount Everest! 🏔️ What an incredible experience!</div>
+                            <div className='post-buttons-div'>
+                                <button className='post-buttons like-btn' type="button">Like</button>
+                                <div className='owner-buttons'>
+                                    <button className='post-buttons edit-btn' type="button">Edit</button>
+                                    <button className='post-buttons delete-btn' type="button">Delete</button>
+                                </div>
+                            </div>
+                        </li>
+                        <li className='post-item'>
+                            <div className='post-header'>
+                                <img className='owner-picture' src="https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww" alt="" />
+                                <div className='post-owner'>John Doe</div>
+                            </div>
+                            <div className='post-text'>Just climbed Mount Everest! 🏔️ What an incredible experience!</div>
+                            <div className='post-buttons-div'>
+                                <button className='post-buttons like-btn' type="button">Like</button>
+                                <div className='owner-buttons'>
+                                    <button className='post-buttons edit-btn' type="button">Edit</button>
+                                    <button className='post-buttons delete-btn' type="button">Delete</button>
+                                </div>
+                            </div>
+                        </li>
+                        <li className='post-item'>
+                            <div className='post-header'>
+                                <img className='owner-picture' src="https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww" alt="" />
+                                <div className='post-owner'>John Doe</div>
+                            </div>
+                            <div className='post-text'>Just climbed Mount Everest! 🏔️ What an incredible experience!</div>
+                            <div className='post-buttons-div'>
+                                <button className='post-buttons like-btn' type="button">Like</button>
+                                <div className='owner-buttons'>
+                                    <button className='post-buttons edit-btn' type="button">Edit</button>
+                                    <button className='post-buttons delete-btn' type="button">Delete</button>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>``
+
             <div className="friends-section">
                 <h2 className='friends-counter'>Friends (248)</h2>
                 <div className='friends-list'>
