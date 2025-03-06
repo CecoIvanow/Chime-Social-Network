547fe208910dad68b0d0414fc9891f8a2eea2b00
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-06 15:49:30 +0200

    Added a hidden logo to the menubar

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,22 +1,23 @@
 import { Link } from "react-router";
 
-        export default function MenuBar() {
-            return <>
-            <nav>
-                <div className="navbar">
-                <div className="logo"><Link to="/">CodingLab</Link></div>
-                    <ul className="menu">
-                        <li><Link to="/" title='Home'><img src="src\static\images\home-icon.png" alt="Home" /></Link></li>
-                        <li><Link to="/profile" title='Profile'><img src="src\static\images\profile-icon.png" alt="Profile" /></Link></li>
-                        <li><Link to="/catalog" title='Catalog'><img src="src\static\images\catalog-icon.png" alt="Catalog" /></Link></li>
-                        <li><Link to="/notifications" title='Notifications'><img src="src\static\images\notifications-icon.png" alt="Notifications" /></Link></li>
-                        <li><Link to="/messages" title='Messages'><img src="src\static\images\messages-icon.png" alt="Messages" /></Link></li>
-                    </ul>
-                    <ul className='auth-menu'>
-                        <li><Link to="/settings" title='Settings'><img src="src\static\images\settings-icon.png" alt="Catalog" /></Link></li>
+export default function MenuBar() {
+    return <>
+        <nav>
+            <div className="navbar">
+                <div className="logo"><Link to="/">Chime</Link></div>
+                <ul className="menu">
+                    <li><Link to="/" title='Home'><img src="src\static\images\home-icon.png" alt="Home" /></Link></li>
+                    {/* <li><Link to="/profile" title='Profile'><img src="src\static\images\profile-icon.png" alt="Profile" /></Link></li> */}
+                    <li><Link to="/catalog" title='Catalog'><img src="src\static\images\catalog-icon.png" alt="Catalog" /></Link></li>
+                    {/* <li><Link to="/notifications" title='Notifications'><img src="src\static\images\notifications-icon.png" alt="Notifications" /></Link></li> */}
+                    {/* <li><Link to="/messages" title='Messages'><img src="src\static\images\messages-icon.png" alt="Messages" /></Link></li> */}
+                </ul>
+                {/* <ul className='auth-menu'>
                         <li><Link to="/logout" title='Logout'><img src="src\static\images\logout-icon.png" alt="Notifications" /></Link></li>
-                    </ul>
-                </div>
-            </nav>
-            </>
-        }
+                        <li><Link to="/settings" title='Settings'><img src="src\static\images\settings-icon.png" alt="Catalog" /></Link></li>
+                        </ul> */}
+                <div className="hidden-logo">Chime</div>
+            </div>
+        </nav>
+    </>
+}
