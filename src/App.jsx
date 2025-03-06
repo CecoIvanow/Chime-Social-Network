52131f594837fbae7938165490a9893709ac9ed6
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-06 15:01:28 +0200

    Added React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import './static/styles/styles.css';
 
-import MenuBar from './components/MenuBar.jsx'
-import LandingPage from './pages/LandingPage.jsx'
-import RegisterPage from './pages/RegisterPage.jsx'
-import UserHomePage from './pages/UserHomePage.jsx'
-import LoginPage from './pages/LoginPage.jsx'
-
 export default function App() {
     return <>
-        <MenuBar />
-        {/* <UserHomePage /> */}
-        {/* <LandingPage /> */}
-        <RegisterPage />
-        {/* <LoginPage /> */}
+
     </>
 }
