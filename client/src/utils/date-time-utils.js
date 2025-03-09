9f4891a708cde9fb63aafa2694350a4990e45644
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-09 15:17:08 +0200

    Added age calculator util

--- client/src/utils/date-time-utils.js
+++ client/src/utils/date-time-utils.js
@@ -1,4 +1,17 @@
+import { differenceInYears } from 'date-fns'
+
 export function memberSinceDateConverter(isoDate) {
     const result = isoDate.split('T').at(0)
     return result;
+}
+
+export function ageCalculator(date) {
+    const [year, month, day] = date.split('-');
+    
+    const birthday = new Date(year, Number(month) - 1, day);
+    const currentDate = new Date();
+    
+    const age = differenceInYears(currentDate, birthday);
+    
+    return age;
 }
