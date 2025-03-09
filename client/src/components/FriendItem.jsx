f45a6880fdd15e859474b2c724d17a42ac19deaf
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-09 16:55:46 +0200

    Style: Changed friend name position in FriendItem component

--- client/src/components/FriendItem.jsx
+++ client/src/components/FriendItem.jsx
@@ -1,7 +1,7 @@
 export default function FriendItem() {
-   return <>
-       <li className='friend-item'>John Doe
-        <img src="https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww" alt="" />
-        </li>
-   </>
+    return <>
+        <li className='friend-item'>
+            <img src="https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww" alt="" />
+        John Doe</li>
+    </>
 }
