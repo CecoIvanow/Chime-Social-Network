78f11821b6bfe7265c4e368e0eea8eb487fbcbc0
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-09 16:01:39 +0200

    css changes to the posts creation input field

--- client/src/pages/UserHomePage.jsx
+++ client/src/pages/UserHomePage.jsx
@@ -23,7 +23,7 @@ export default function UserHomePage() {
                         <div className='post-header'>
                             <img src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" />
                             <label htmlFor="post"></label>
-                            <input type="text" name="post" id="post" />
+                            <input type="text" name="post" id="post" placeholder="Share your thoughts..."/>
                         </div>
                         <button className='submit-button' >Post</button>
                     </form>
