202c206aa9375c3ab1f49af1a768f3f16ab15877
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-09 02:12:54 +0200

    Added :userId param to the profile URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,7 @@ export default function App() {
 
             {/* User only pages */}
             <Route path='/settings' element={isUser ? <SettingsPage /> : <Navigate to="/login" />} />
-            <Route path='/profile' element={isUser ? <ProfilePage /> : <Navigate to="/login" />} />
+            <Route path='/profile/:userId' element={isUser ? <ProfilePage /> : <Navigate to="/login" />} />
             <Route path='/logout' element={isUser ? <Logout setIsUser={setIsUser} /> : <Navigate to="/" />} />
 
             {/* Guest only pages */}
