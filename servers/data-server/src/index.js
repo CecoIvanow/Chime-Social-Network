76b31fd7e930c99f432cfbd509858471788a6f24
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-14 01:43:11 +0200

    Included PATCH method in CORS headers access

--- servers/data-server/src/index.js
+++ servers/data-server/src/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next();
 })
 
