97fdc6d523a657529a09a3561a28e3e0b10a0d12
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-13 18:53:26 +0200

    Server/Refactor: Removed unused function

--- servers/data-server/src/repositories/post-repositries.js
+++ servers/data-server/src/repositories/post-repositries.js
@@ -2,18 +2,6 @@ import Post from "../models/Post.js";
 
 const COMMONLY_NEEDED_PARAMS = 'firstName lastName imageUrl'
 
-async function fetchAllWithOwners() {
-    const allPosts = await Post
-        .find({})
-        .populate({
-            path: 'owner',
-            select: COMMONLY_NEEDED_PARAMS
-        })
-        .lean();
-
-    return allPosts
-}
-
 async function create(postData) {
     const newPost = await Post.create(postData);
 
