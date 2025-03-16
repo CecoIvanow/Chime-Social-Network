2d29dfa43099cab5b9e47da95eaf2b3f550dbad9
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-16 16:06:44 +0200

    Client/Feature: Made comment creation endpoint call functions associated with comment creation

--- servers/data-server/src/controllers/comment-controller.js
+++ servers/data-server/src/controllers/comment-controller.js
@@ -1,10 +1,22 @@
 import { Router } from 'express'
+import commentRepositories from '../repositories/comment-repositories.js';
+import postRepositories from '../repositories/post-repositries.js';
 
 const commentController = Router();
 
 commentController.post('/comments', async (req, res) => {
-    console.log('Works');
-    res.end()
+    const commentData = req.body;
+
+    try {
+        const newComment = await commentRepositories.create(commentData);
+
+        await postRepositories.attachCommentToPost(commentData.onPost, newComment._id);
+
+        res.json(newComment);
+        res.end();
+    } catch (error) {
+        console.error(error.message);
+    }
 })
 
 export default commentController;
