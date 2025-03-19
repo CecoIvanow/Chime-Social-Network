86893e95e1c0427ddc054072e9969465da51c6fa
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-19 19:26:23 +0200

    Separated the SettingsPage into components

--- client/src/components/pages/settings-page/SettingsPage.jsx
+++ client/src/components/pages/settings-page/SettingsPage.jsx
@@ -3,7 +3,9 @@ import { useNavigate } from 'react-router'
 
 
 import userServices from "../../../services/user-services";
-import Button from "../../ui/button/Button";
+.0
+import PasswordChangeForm from "./password-change-form/PasswordChangeForm";
+import EmailChangeForm from "./email-change-form/EmailChangeForm";
 
 export default function SettingsPage({
     userId
@@ -52,67 +54,15 @@ export default function SettingsPage({
 
     return <>
         <div className="settings-container">
-            <form onSubmit={onEmailChangeSubmitHandler}>
-                <div className="settings-card password-section">
-                    <h2 className="settings-heading">Account Email - {userEmail}</h2>
+            <EmailChangeForm
+                userEmail={userEmail}
+                onSubmitHandler={onEmailChangeSubmitHandler}
+            />
 
-                    <div className="form-group">
-                        <label className="form-label">Current Email</label>
-                        <input type="text" className="form-input" name="curEmail" />
-                    </div>
+            <PasswordChangeForm
+                onSubmitHandler={onPasswordChangeSubmitHandler}
+            />
 
-                    <div className="form-group">
-                        <label className="form-label">New Email</label>
-                        <input type="text" className="form-input" name="newEmail" />
-                    </div>
-
-                    <div className="form-group">
-                        <label className="form-label">Current Password</label>
-                        <input type="password" className="form-input" name="curPass" />
-                    </div>
-
-                    <div className="form-group">
-                        <label className="form-label">Repeat Password</label>
-                        <input type="password" className="form-input" name="rePass" />
-                    </div>
-
-                    <Button
-                        btnStyle="save-button"
-                        buttonName="Change Email"
-                    />
-                </div>
-            </form>
-
-            <form onSubmit={onPasswordChangeSubmitHandler}>
-                <div className="settings-card password-section">
-                    <h2 className="settings-heading">Account Password - ******</h2>
-
-                    <div className="form-group">
-                        <label className="form-label">Current Email</label>
-                        <input type="text" className="form-input" name="curEmail" />
-                    </div>
-
-                    <div className="form-group">
-                        <label className="form-label">Current Password</label>
-                        <input type="password" className="form-input" name="curPass" />
-                    </div>
-
-                    <div className="form-group">
-                        <label className="form-label">New Password</label>
-                        <input type="password" className="form-input" name="newPass" />
-                    </div>
-
-                    <div className="form-group">
-                        <label className="form-label">Repeat New Password</label>
-                        <input type="password" className="form-input" name="rePass" />
-                    </div>
-
-                    <Button
-                        btnStyle="save-button"
-                        buttonName="Change Password"
-                    />
-                </div>
-            </form>
         </div>
     </>
 }
