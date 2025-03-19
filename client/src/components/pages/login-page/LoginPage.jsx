d8a5c6fa539d1c89d14d5e3508bbb2776e0336bb
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-19 23:24:49 +0200

    Client/Refactor: Separated the Login page into components

--- client/src/components/pages/login-page/LoginPage.jsx
+++ client/src/components/pages/login-page/LoginPage.jsx
@@ -1,5 +1,7 @@
-import { Link } from "react-router";
 import userServices from "../../../services/user-services";
+
+import AuthButton from "../../shared/auth-button/AuthButton";
+import AuthNavLink from "../../shared/auth-nav-link/AuthNavLink";
 
 export default function LoginPage({
     setIsUser
@@ -17,30 +19,31 @@ export default function LoginPage({
     return <>
         <div className="login-page">
             <div className="container">
-                {/* <!-- Title section --> */}
+
                 <div className="title">Login</div>
                 <div className="content">
-                    {/* <!-- Registration form --> */}
+
                     <form onSubmit={submitFormHandler}>
                         <div className="user-details">
-                            {/* <!-- Input for Email --> */}
+
                             <div className="input-box">
                                 <span className="details">Email</span>
                                 <input type="text" placeholder="Enter your email" name="email" required />
                             </div>
-                            {/* <!-- Input for Password --> */}
+
                             <div className="input-box">
                                 <span className="details">Password</span>
                                 <input type="password" placeholder="Enter your password" name="password" required />
                             </div>
                         </div>
-                        {/* <!-- Submit button --> */}
-                        <div className="button-auth">
-                            <input type="submit" value="Login" />
-                        </div>
-                        <div className='to-register'>
-                            <Link to="/register">Don`t have an account?</Link>
-                        </div>
+
+                        <AuthButton
+                            buttonText="Login"
+                        />
+                        <AuthNavLink
+                            path="/register"
+                            buttonText="Don`t have an account?"
+                        />
                     </form>
                 </div>
             </div>
