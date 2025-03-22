95faae5627f9e4fbe34a811fad4314fa3a5af740
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-22 21:11:36 +0200

    Client/Refactor: Extracted profile data section into a separate component

--- client/src/components/pages/profile-page/ProfilePage.jsx
+++ client/src/components/pages/profile-page/ProfilePage.jsx
@@ -7,6 +7,7 @@ import PostItem from "../../shared/post/post-item/PostItem";
 import PostCreateForm from "../../shared/post/post-create-form/PostCreateForm";
 import LinkButton from "../../ui/buttons/link-button/LinkButton";
 import SectionHeading from "../../ui/headings/SectionHeading";
+import ProfileDataSection from "../../shared/profile/ProfileDataSection";
 
 export default function ProfilePage({
     isUser
@@ -35,32 +36,12 @@ export default function ProfilePage({
 
     return <>
         <div className="profile-container">
-            <div className="profile-info-section">
-                <div className="profile-header">
-                    <img src={userData.imageUrl} className="profile-avatar" alt="Profile picture" />
-                    <div className="profile-info">
-                        <h2>{(userData.firstName)} {(userData.lastName)}</h2>
-                        <p><span className="info-label">Bio:</span> {userData.bio ? userData.bio : 'N\\A'}</p>
-                        <p><span className="info-label">Age:</span> {userData.age || userData.age === 0 ? userData.age : 'N\\A'}</p>
-                        <p><span className="info-label">Gender:</span> {userData.gender ? userData.gender : 'N\\A'}</p>
-                        <p><span className="info-label">Location:</span> {userData.location ? userData.location : 'N\\A'}</p>
-                        <p><span className="info-label">Occupation:</span> {userData.occupation ? userData.occupation : 'N\\A'}</p>
-                        <p><span className="info-label">Education:</span> {userData.education ? userData.education : 'N\\A'}</p>
-                        <p><span className="info-label">Status:</span> {userData.status ? userData.status : 'N\\A'}</p>
-                        <p><span className="info-label">Member Since:</span> {userData.memberSince ? userData.memberSince : 'N\\A'}</p>
-                        {isUser && (
-                            <LinkButton
-                                urlLink={'/edit'}
-                                btnStyle="edit-profile-btn"
-                                buttonName="Edit Profile"
-                            />
-                        )}
-                    </div>
-                </div>
-            </div>
+            <ProfileDataSection
+                isUser={isUser}
+                userData={userData}
+            />
 
             <div className="posts-section">
-
                 <SectionHeading
                     sectionName={isUser === userData._id ? 'My Posts:' : `${userData.firstName}'s Posts:`}
                 />
