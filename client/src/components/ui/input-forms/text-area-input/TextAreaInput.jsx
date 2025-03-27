f4a981d630b99af2a2f64b32c724e88f47f4a781
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-27 23:40:34 +0200

    Client/Fix: Added missing name attribute to TextAreaInput component

--- client/src/components/ui/input-forms/text-area-input/TextAreaInput.jsx
+++ client/src/components/ui/input-forms/text-area-input/TextAreaInput.jsx
@@ -1,12 +1,12 @@
 export default function TextAreaInput({
     value,
-    fieldName
-
+    fieldName,
+    inputName
 }) {
     return <>
         <div className="form-group">
             <label className="form-label">{fieldName}</label>
-            <textarea className="edit-textarea">{value}</textarea>
+            <textarea className="edit-textarea" name={inputName}>{value}</textarea>
         </div>
     </>
 }
