e611e6a8bf1abb7c73cb0254db30cdbc4655fed9
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-30 20:18:19 +0300

    Client/Feature: Added button for reloading the page

--- client/src/components/layout/error-boundary/error-message/ErrorMessage.jsx
+++ client/src/components/layout/error-boundary/error-message/ErrorMessage.jsx
@@ -1,3 +1,5 @@
+import Button from "../../../ui/buttons/button/Button";
+
 export default function ErrorMessage() {
     return <>
         <div className="error">
@@ -5,6 +7,12 @@ export default function ErrorMessage() {
                 <div className="error-icon"></div>
                 <h1 className="header-message">Oops, something went wrong.</h1>
                 <p className="paragraph-message">Please reload the page.</p>
+
+                <Button
+                    onClickHandler={() => window.location.reload()}
+                    buttonName="Reload"
+                    btnStyle="button comment-btn"
+                />
             </div>
         </div>
     </>
