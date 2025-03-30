cd5b864c32a824808d8153271c2e473c6889ff8d
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-30 21:52:33 +0300

    Client/Feature: Implementerd a 2 second wait on screen time duration for alert notification

--- client/src/components/ui/alert-notification/AlertNotification.jsx
+++ client/src/components/ui/alert-notification/AlertNotification.jsx
@@ -1,4 +1,17 @@
+import { useContext, useEffect } from "react"
+import { AlertContext } from "../../../contexts/alert-context"
+
 export default function AlertNotification() {
+    const { alert, setAlert } = useContext(AlertContext);
+
+    useEffect(() => {
+        if (alert) {
+            setTimeout(() => {
+                setAlert(false);
+            }, 2000)
+        }
+    }, [alert, setAlert])
+
     return <>
         <div className="error-notification">
             <div className="error-icon"></div>
