f53b99fd4e90db01e63d352e659f9d7bdac73775
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-31 01:10:29 +0300

    Client/Fix: Inserted back accidentally deleted AuthForm required attribute

--- client/src/components/shared/auth/auth-form/AuthForm.jsx
+++ client/src/components/shared/auth/auth-form/AuthForm.jsx
@@ -21,6 +21,7 @@ export default function AuthForm({
                 name={inputName}
                 defaultValue={value}
                 onChange={onValueChangeHandler}
+                required
             />
         </div>
     </>
