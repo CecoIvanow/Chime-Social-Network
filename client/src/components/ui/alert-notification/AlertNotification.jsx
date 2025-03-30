7032e73295eff36532d502ad7ccd58775b99a12e
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-31 01:07:47 +0300

    Client/Feature: Implemented AlernNotification component rerender and timeout restart on alert change

--- client/src/components/ui/alert-notification/AlertNotification.jsx
+++ client/src/components/ui/alert-notification/AlertNotification.jsx
@@ -1,14 +1,19 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useRef } from "react"
 import { AlertContext } from "../../../contexts/alert-context"
 
 export default function AlertNotification() {
     const { alert, setAlert } = useContext(AlertContext);
+    const curTimeoutIdRef = useRef(null);
 
     useEffect(() => {
         if (alert) {
-            setTimeout(() => {
+            if (curTimeoutIdRef.current) {
+                clearTimeout(curTimeoutIdRef.current);
+            }
+
+            curTimeoutIdRef.current = setTimeout(() => {
                 setAlert(false);
-            }, 5000)
+            }, 5000);
         }
     }, [alert, setAlert])
 
