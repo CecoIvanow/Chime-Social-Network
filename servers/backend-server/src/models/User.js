44108e3c0d0510e2aac4f80c3a27042649041256
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-31 00:42:49 +0300

    Server/Refactor: Added pre validation sequential validation checks

--- servers/backend-server/src/models/User.js
+++ servers/backend-server/src/models/User.js
@@ -3,27 +3,27 @@ import { Schema, Types, model } from "mongoose";
 const userSchema = new Schema({
     email: {
         type: String,
-        // required: [true, 'Email is requred!'],
+        required: [true, 'Email is required!'],
     },
     password: {
         type: String,
-        // required: [true, 'Password is required!'],
+        required: [true, 'Password is required!'],
     },
     firstName: {
         type: String,
-        // required: [true, 'First name is required!']
+        required: [true, 'First name is required!'],
     },
     lastName: {
         type: String,
-        // required: [true, 'Last name is required!']
+        required: [true, 'Last name is required!'],
     },
     birthday: {
         type: String,
-        // required: [true, 'Birthday is required!']
+        required: [true, 'Birthday is required!'],
     },
     gender: {
         type: String,
-        // required: [true, 'Gender is required!']
+        required: [true, 'Gender is required!'],
     },
     imageUrl: {
         type: String,
@@ -57,6 +57,30 @@ const userSchema = new Schema({
         type: Types.ObjectId,
         ref: 'Post',
     }]
+})
+
+userSchema.pre('validate', function (next) {
+    if (!this.firstName) {
+        return next(new Error('First name is required!'))
+
+    } else if (!this.lastName) {
+        return next(new Error('Last name is required!'))
+
+    } else if (!this.email) {
+        return next(new Error('Email is required!'))
+
+    } else if (!this.birthday) {
+        return next(new Error('Birthday is required!'))
+
+    } else if (!this.password) {
+        return next(new Error('Password is required!'))
+
+    } else if (!this.gender) {
+        return next(new Error('Gender is required!'))
+
+    }
+    
+    next();
 })
 
 const User = model('User', userSchema);
