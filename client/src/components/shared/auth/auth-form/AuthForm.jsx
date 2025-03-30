8b61facb8d87aa8adc52e788725a152106507dd9
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-03-31 01:06:31 +0300

    Client/Feature: Implemented value state to AuthForm input component for rerender persistence

--- client/src/components/shared/auth/auth-form/AuthForm.jsx
+++ client/src/components/shared/auth/auth-form/AuthForm.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
+
 export default function AuthForm({
     fieldName,
     inputName,
     inputType,
     placeholderText,
 }) {
+    const [value, setValue] = useState('');
+
+    const onValueChangeHandler = (e) => {
+        setValue(e.currentTarget.value);
+    }
+
     return <>
         <div className="input-box">
             <span className="details">{fieldName}</span>
-            <input type={inputType} placeholder={`Enter your ${placeholderText}`} name={inputName} required />
+            <input
+                type={inputType}
+                placeholder={`Enter your ${placeholderText}`}
+                name={inputName}
+                defaultValue={value}
+                onChange={onValueChangeHandler}
+            />
         </div>
     </>
 }
