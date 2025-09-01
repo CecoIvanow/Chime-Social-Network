19f3ab25ef152a27d0e8c2a4fbda96e5801d8885
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-09-01 23:37:32 +0300

    Client/Test: add unit tests for PostHeader in /shared/post

---
+++ client/src/components/shared/post/post-header/PostHeader.spec.jsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+
+import PostHeader from "./PostHeader.jsx";
+
+import { PostContext } from "../../../../contexts/post-context.js";
+import { BrowserRouter, MemoryRouter } from "react-router";
+
+const postMock = {
+    owner: {
+        _id: 'owner123',
+        imageUrl: 'https://example.org/test-123',
+        firstName: 'John',
+        lastName: 'Doe',
+    },
+    _id: 'post056',
+    postedOn: '02.05.2025',
+}
+
+describe("PostHeader component", () => {
+    it("renders with post data", () => {
+        render(
+            <MemoryRouter>
+                <PostContext.Provider value={{ post: postMock }}>
+                    <PostHeader />
+                </PostContext.Provider>
+            </MemoryRouter>
+        );
+
+        expect(screen.getByTestId('owner-image')).toHaveAttribute('src', postMock.owner.imageUrl);
+
+        expect(screen.getByTestId('profile-link')).toHaveAttribute('href', `/profile/${postMock.owner._id}`);
+        expect(screen.getByTestId('profile-link')).toHaveTextContent(`${postMock.owner.firstName} ${postMock.owner.lastName}`);
+        
+        expect(screen.getByTestId('post-link')).toHaveAttribute('href', `/post/${postMock._id}/details`);
+        expect(screen.getByTestId('post-link')).toHaveTextContent(`Posted on ${postMock.postedOn}`);
+    });
+
+});
