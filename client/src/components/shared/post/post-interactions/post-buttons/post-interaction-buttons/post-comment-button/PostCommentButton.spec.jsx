f7c807cef2abb30cea29f6c0cb88c551de58fdd4
Cvetelyn <cvetelyn.ivanov@gmail.com>
2025-12-15 15:30:01 +0200

    client/test: add unit tests for PostCommentButton in /shared/post

---
+++ client/src/components/shared/post/post-interactions/post-buttons/post-interaction-buttons/post-comment-button/PostCommentButton.spec.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+
+import PostCommentButton from "./PostCommentButton";
+
+import { PostContext } from "../../../../../../../contexts/post-context";
+
+const post = {
+    _id: 1
+}
+
+vi.mock("../../../../../../ui/buttons/link-button/LinkButton", () => ({
+    default: ({ urlLink }) => (
+        <div data-testid='urlLink'>{urlLink}</div>
+    )
+}));
+
+describe("PostCommentButton component", () => {
+    it("renders LinkButton with correct urlLink prop", () => {
+        render(
+            <PostContext.Provider value={{ post }}>
+                <PostCommentButton />
+            </PostContext.Provider >
+        );
+
+        expect(screen.getByTestId('urlLink')).toHaveTextContent(`/post/${post._id}/details`);
+    });
+
+})
