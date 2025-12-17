b451fc8c61b712c47b9f12de65aa501c9d58e536
Cvetelyn <cvetelyn.ivanov@gmail.com>
2025-12-17 18:44:39 +0200

    client/test: add test for correct render amount of FriendItem in FriendsList component

---
+++ client/src/components/pages/user-home-page/friends-section/friends-list/FriendsList.spec.jsx
@@ -0,0 +1,22 @@
+import { fireEvent, getAllByTestId, render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+
+import FriendsList from "./FriendsList";
+
+vi.mock("./friend-item/FriendItem", () => ({
+    default: ({ friend }) => <div data-testid="friend-item">{friend}</div>
+}))
+
+describe("FriendsList component", () => {
+    const matchingFriends = ["friend1", "friend2"];
+
+    it("renders the correct number of FriendItem components", () => {
+        render(
+            <FriendsList
+                matchingFriends={matchingFriends}
+            />
+        );
+
+        expect(screen.getAllByTestId("friend-item")).toHaveLength(matchingFriends.length);
+    });
+});
