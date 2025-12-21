17169e7d31d05a4bf4c8c63e9a96af1ea73ba017
Cvetelyn <cvetelyn.ivanov@gmail.com>
2025-12-21 19:01:16 +0200

    client/test: add conditional render tests for PostsSection based on loading state in ProfilePage

--- client/src/components/pages/profile-page/ProfilePage.spec.jsx
+++ client/src/components/pages/profile-page/ProfilePage.spec.jsx
@@ -1,5 +1,6 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
+import { useContext } from "react";
 
 import ProfilePage from "./ProfilePage";
 
@@ -22,11 +23,19 @@ vi.mock("../../shared/profile/profile-section/ProfileSection", () => ({
 
 vi.mock("../../shared/post/posts-section/PostsSection", () => ({
     default: ({ isLoading, userName }) => <>
-        {isLoading ? (
-            <div data-testid="posts-loading-spinner"></div>
-        ) : (
-            <div data-testid="posts-section">{userName}</div>
-        )};
+        <TotalPostsContext.Consumer>
+            {context => (
+                isLoading ? (
+                    <div data-testid="posts-loading-spinner" ></div>
+                ) : (
+                    <>
+                        <div data-testid="posts-section">{userName}</div>
+                            {context.totalPosts.map(post =>
+                            <div key={post._id} data-testid="post"></div>)}
+                    </>
+                )
+            )}
+        </TotalPostsContext.Consumer>
     </>
 }));
 
@@ -37,10 +46,12 @@ describe("ProfilePage component", () => {
     const getUserPosts = vi.fn();
 
     const userData = { firstName: "Petar" };
-    const userPosts = [
-        { _id: 1, content: "First Post" },
-        { _id: 2, content: "Second Post" },
-    ];
+    const userPosts = {
+        createdPosts: [
+            { _id: 2},
+            { _id: 1},
+        ]
+    }
 
     function renderComp(
         options = {
@@ -74,7 +85,7 @@ describe("ProfilePage component", () => {
     it.each([
         { isLoading: false, renderedComp: "ProfileSection" },
         { isLoading: true, renderedComp: "isLoading" },
-    ])("passes props and on isLoading $isLoading renders $renderedComp", async ({isLoading}) => {
+    ])("passes props and on isLoading $isLoading renders $renderedComp", async ({ isLoading }) => {
         const pattern = new RegExp(`^${userData.firstName}$`)
 
         renderComp({
@@ -83,7 +94,7 @@ describe("ProfilePage component", () => {
             getUserPosts: true,
         });
 
-        if(isLoading) {
+        if (isLoading) {
             expect(screen.queryByTestId("profile-section")).not.toBeInTheDocument();
             expect(screen.getByTestId("profile-loading-spinner")).toBeInTheDocument();
         } else {
@@ -91,6 +102,30 @@ describe("ProfilePage component", () => {
                 expect(screen.getByTestId("profile-section")).toHaveTextContent(pattern);
             })
             expect(screen.queryByTestId("profile-loading-spinner")).not.toBeInTheDocument();
+        };
+    });
+
+    it.each([
+        { isLoading: false, renderedComp: "PostsSection" },
+        { isLoading: true, renderedComp: "isLoading" },
+    ])("passes props and on isLoading $isLoading renders $renderedComp", async ({ isLoading }) => {
+        const pattern = new RegExp(`^${userData.firstName}$`)
+
+        renderComp({
+            isLoading,
+            getUserData: true,
+            getUserPosts: true,
+        });
+
+        if (isLoading) {
+            expect(screen.queryByTestId("posts-section")).not.toBeInTheDocument();
+            expect(screen.getByTestId("posts-loading-spinner")).toBeInTheDocument();
+        } else {
+            await waitFor(() => {
+                expect(screen.getByTestId("posts-section")).toHaveTextContent(pattern);
+                expect(screen.getAllByTestId("post")).toHaveLength(userPosts.createdPosts.length);
+            });
+            expect(screen.queryByTestId("posts-loading-spinner")).not.toBeInTheDocument();
         };
     });
 });
