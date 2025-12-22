e232f32d63636856012e1f6a25df95b9169d3fdd
Cvetelyn <cvetelyn.ivanov@gmail.com>
2025-12-22 21:40:56 +0200

    client/test: add image upload and image preview test in ImageUpload

--- client/src/components/pages/profile-edit-page/image-upload/ImageUpload.spec.jsx
+++ client/src/components/pages/profile-edit-page/image-upload/ImageUpload.spec.jsx
@@ -1,11 +1,23 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { describe, expect, it, vi, beforeEach } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, it, vi, beforeAll } from "vitest";
 
 import ImageUpload from "./ImageUpload";
+
+beforeAll(() => {
+    const global = window;
+    
+    global.URL.createObjectURL = vi.fn(() => "blob:mock-image-url");
+    global.URL.revokeObjectURL = vi.fn();
+})
 
 describe("ImageUpload component", () => {
     const imageUrlMock = "https://firebase.mock/avatar.webp";
     const setImageUploadMock = vi.fn();
+    const imageFile = new File(
+        ["bits"],
+        "avatar.png",
+        {type: "image/png"}
+    );
 
     function renderComp() {
         render(
@@ -14,11 +26,20 @@ describe("ImageUpload component", () => {
                 setImageUpload={setImageUploadMock}
             />
         );
-    }
+    };
 
     it("renders ImageUpload with passed props", () => {
         renderComp();
 
         expect(screen.getByAltText("Profile picture")).toHaveAttribute("src", imageUrlMock);
+    });
+
+    it("uploads image and shows image preview", () => {
+        renderComp();
+
+        fireEvent.change(screen.getByTestId("image-input"), {target: {files: [imageFile]}});
+
+        expect(setImageUploadMock).toHaveBeenCalledWith(imageFile);
+        expect(screen.getByAltText("Profile picture")).toHaveAttribute("src", "blob:mock-image-url");
     })
-})
+});
