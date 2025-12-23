6af6b0fc8499f6e3172a787a1ebd530f32314341
Cvetelyn <cvetelyn.ivanov@gmail.com>
2025-12-23 15:29:50 +0200

    client/test: add on change trigger test for onTextChangeHandler in CommentEditTextArea

--- client/src/components/pages/post-details-page/comments-section/comment-items-list/comment-item/comment-edit-textarea/CommentEditTextArea.spec.jsx
+++ client/src/components/pages/post-details-page/comments-section/comment-items-list/comment-item/comment-edit-textarea/CommentEditTextArea.spec.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it, vi, beforeEach } from "vitest";
 
 import CommentEditTextArea from "./CommentEditTextArea";
@@ -26,5 +26,11 @@ describe("CommentEditTextArea component", () => {
     it("renders component with context values", () => {
         expect(screen.getByRole("textbox")).toHaveAttribute("placeholder", PLACEHOLDER_TEXT);
         expect(screen.getByRole("textbox")).toHaveValue(onEditCommentText);
-    })
+    });
+
+    it("triggers onTextChangeHandler on value change", () => {
+        fireEvent.change(screen.getByRole("textbox"), {target: {value: "a"}});
+
+        expect(onTextChangeHandler).toHaveBeenCalled();
+    });
 });
