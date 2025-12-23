a5a40039fb85b25da1e56f78807c8aab7fa39851
Cvetelyn <cvetelyn.ivanov@gmail.com>
2025-12-23 14:37:17 +0200

    client/test: add render test for CommentItemHeader

---
+++ client/src/components/pages/post-details-page/comments-section/comment-items-list/comment-item/comment-item-header/CommentItemHeader.spec.jsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi, beforeEach } from "vitest";
+import { MemoryRouter } from "react-router";
+
+import CommentItemHeader from "./CommentItemHeader";
+
+
+
+const comment = {
+    owner: {
+        _id: "ownerId",
+        imageUrl: "https://example.org/avatar.webp",
+        firstName: "Petar",
+        lastName: "Ivanov",
+    },
+    postedOn: "23.12.2025",
+}
+
+function setup() {
+    render(
+        <MemoryRouter>
+            <CommentItemHeader comment={comment} />
+        </MemoryRouter>
+    );
+}
+
+describe("CommentItemHeader component", () => {
+    beforeEach(() => {
+        setup();
+    });
+
+    it("renders component with passed props", () => {
+        expect(screen.getByAltText(`${comment.owner.firstName} ${comment.owner.lastName} avatar`)).toHaveAttribute("src", comment.owner.imageUrl);
+        expect(screen.getByRole("link")).toHaveTextContent(`${comment.owner.firstName} ${comment.owner.lastName}`);
+        expect(screen.getByRole("link")).toHaveAttribute("href", `/profile/${comment.owner._id}`);
+        expect(screen.getByText(`Posted on ${ comment.postedOn }`)).toBeInTheDocument();
+    });
+});
