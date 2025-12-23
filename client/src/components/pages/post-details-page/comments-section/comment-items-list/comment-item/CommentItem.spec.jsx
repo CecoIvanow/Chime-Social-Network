fa9479856a7426fce4918cc0d4109da3cc87569a
Cvetelyn <cvetelyn.ivanov@gmail.com>
2025-12-23 17:18:09 +0200

    client/test add CommentItemHeader render test in CommentItem

---
+++ client/src/components/pages/post-details-page/comments-section/comment-items-list/comment-item/CommentItem.spec.jsx
@@ -0,0 +1,54 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, it, vi, beforeEach } from "vitest";
+
+import CommentItem from "./CommentItem";
+
+import { ActionsContext } from "../../../../../../contexts/actions-context";
+
+vi.mock("./comment-item-header/CommentItemHeader", () => ({
+    default: ({ comment }) => <div data-testid="comment-item-header">{comment.text}</div>
+}))
+
+vi.mock("./comment-edit-textarea/CommentEditTextArea", () => ({
+    default: () => <div data-testid="comment-edit-text-area"></div>
+}));
+
+vi.mock("./comment-text/CommentText", () => ({
+    default: () => <div data-testid="comment-text"></div>
+}));
+
+vi.mock("./comment-buttons/CommentButtons", () => ({
+    default: ({ comment }) => <div data-testid="comment-button">{comment.text}</div>
+}))
+
+const comment = {
+    text: "This is a comment."
+};
+
+const isEditClicked = false;
+const setCommentText = vi.fn();
+const setOnEditCommentText = vi.fn();
+
+function setup(options = {
+    isEditClicked,
+}) {
+    const isEditClicked = options.isEditClicked || false;
+
+    render(
+        <ActionsContext.Provider value={{
+            isEditClicked,
+            setCommentText,
+            setOnEditCommentText
+        }}>
+            <CommentItem comment={comment} />
+        </ActionsContext.Provider>
+    );
+};
+
+describe("CommentItem component", () => {
+    it("renders CommentItemHeader with passed props", () => {
+        setup();
+
+        expect(screen.getByTestId("comment-item-header")).toHaveTextContent(comment.text);
+    });
+});
