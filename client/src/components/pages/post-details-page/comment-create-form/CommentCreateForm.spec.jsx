44b254be54df873d099d2e2197adc6be2f772f1e
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-12 00:56:41 +0200

    client/test: add trigger test for setAlert on rejected createComment call in CommentCreateForm

--- client/src/components/pages/post-details-page/comment-create-form/CommentCreateForm.spec.jsx
+++ client/src/components/pages/post-details-page/comment-create-form/CommentCreateForm.spec.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { describe, expect, it, vi, beforeEach } from "vitest";
 
 import CreateCommentForm from "./CommentCreateForm";
@@ -8,19 +8,23 @@ import { UserContext } from "../../../../contexts/user-context";
 import { AlertContext } from "../../../../contexts/alert-context";
 
 vi.mock("../../../shared/input-fields/create-content-input-field/CreateContentInputField", () => ({
-    default: ({ onSubmitHandler, onTextChangeHandler, buttonText, commentText }) => <>
-        <form>
+    default: ({ onSubmitHandler, onTextChangeHandler, buttonText, text }) => <>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            const formData = new FormData(e.currentTarget);
+            onSubmitHandler(formData);
+        }}>
             <input
                 type="text"
-                onSubmit={onSubmitHandler}
                 onChange={onTextChangeHandler}
                 id="comment"
-                value={commentText}
+                value={text}
                 data-testid="input"
+                name="content"
             >
             </input>
 
-            <button data-testid="button">{buttonText}</button>
+            <button data-testid="button" type="submit">{buttonText}</button>
         </form>
     </>
 }))
@@ -54,12 +58,14 @@ const newComment = {
 const BUTTON_TEXT = "Reply";
 const INITIAL_INPUT_VALUE = "";
 
+const CREATE_COMMENT_ERROR_MSG = "Successfully rejected createComment call!"
+
 function setup(options = {
     createCommentSuccess: true,
     createCommentTruthyReturn: true
 }) {
-    if(!options.createCommentSuccess) {
-        createCommentMock.mockRejectedValue(new Error("Successfully rejected createComment call!"));
+    if (!options.createCommentSuccess) {
+        createCommentMock.mockRejectedValue(new Error(CREATE_COMMENT_ERROR_MSG));
     } else if (options.createCommentTruthyReturn) {
         createCommentMock.mockResolvedValue(newComment);
     } else {
@@ -67,8 +73,8 @@ function setup(options = {
     }
 
     render(
-        <AlertContext.Provider value={setAlert}>
-            <UserContext.Provider value={isUser}>
+        <AlertContext.Provider value={{ setAlert }}>
+            <UserContext.Provider value={{ isUser }}>
                 <PostContext.Provider value={{ post, setPost }}>
                     <CreateCommentForm />
                 </PostContext.Provider>
@@ -92,8 +98,20 @@ describe("CommentCreateForm component", () => {
 
         const inputEl = screen.getByTestId("input");
 
-        fireEvent.change(inputEl, {target: {value: newValue}});
+        fireEvent.change(inputEl, { target: { value: newValue } });
 
         expect(inputEl).toHaveValue(newValue);
     });
+
+    it("on rejected createComment call triggers setAlert", async () => {
+        setup({
+            createCommentSuccess: false
+        });
+
+        fireEvent.click(screen.getByTestId("button"));
+
+        await waitFor(() => {
+            expect(setAlert).toHaveBeenCalledWith(CREATE_COMMENT_ERROR_MSG);
+        })
+    })
 });
