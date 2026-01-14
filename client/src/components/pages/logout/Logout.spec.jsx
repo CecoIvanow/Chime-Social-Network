ce97e7a1f09fda9ecc0423405b314ec888b3a6a4
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-14 20:04:42 +0200

    client/tests: add logout call test on render in Logout

--- client/src/components/pages/logout/Logout.spec.jsx
+++ client/src/components/pages/logout/Logout.spec.jsx
@@ -1,12 +1,41 @@
-import { fireEvent, getByTestId, render, screen, waitFor } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi, beforeEach } from "vitest";
+
+import Logout from "./Logout";
+
+import { AlertContext } from "../../../contexts/alert-context";
+
+vi.mock("../../../hooks/useUserServices", () => ({
+    default: () => ({
+        logout: logoutMock
+    })
+}));
+
+const LOGOUT_ERR_MSG = "Successfully rejected logout call!";
+
+const logoutMock = vi.fn();
+
+const setAlert = vi.fn();
+
+function setup(options = {
+    logoutRejectedCall: false
+}) {
+
+    options.logoutRejectedCall ?
+    logoutMock.mockResolvedValue() :
+    logoutMock.mockRejectedValue(new Error(LOGOUT_ERR_MSG));
+
+    render(
+        <AlertContext.Provider value={{ setAlert }}>
+            <Logout />
+        </AlertContext.Provider>
+    );
+}
 
 describe("Logout component", () => {
     it("triggers logout on initial render", () => {
-        
-    });
+        setup();
 
-    it("name", () => {
-        
+        expect(logoutMock).toHaveBeenCalled();
     });
 });
