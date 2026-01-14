6fa4ab4bc94243194bced031e12b74f91e55a77c
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-14 19:45:24 +0200

    client/ref: add AlertContext usage for error handling in Logout component

--- client/src/components/pages/logout/Logout.jsx
+++ client/src/components/pages/logout/Logout.jsx
@@ -1,12 +1,18 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 
 import useUserServices from "../../../hooks/useUserServices";
 
+import { AlertContext } from "../../../contexts/alert-context";
+
 export default function Logout() {
     const { logout } = useUserServices();
+    const { setAlert } = useContext(AlertContext);
 
     useEffect(() => {
         logout()
-            .catch(error => console.error(error.message));
-    }, [logout])
+            .catch(error => {
+                console.error(error);
+                setAlert(error.message);
+            });
+    }, [logout, setAlert])
 }
---
+++ client/src/components/pages/logout/Logout.spec.jsx
@@ -0,0 +1,12 @@
+import { fireEvent, getByTestId, render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+
+describe("Logout component", () => {
+    it("triggers logout on initial render", () => {
+        
+    });
+
+    it("name", () => {
+        
+    });
+});
