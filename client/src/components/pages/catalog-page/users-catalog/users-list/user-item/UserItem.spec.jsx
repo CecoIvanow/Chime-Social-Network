8aa1f463ec8f652c9ea4ba56b14a7bb41b70c3f0
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-16 18:34:08 +0200

    client/test: add handleAddFriend trigger test in UserItem

--- client/src/components/pages/catalog-page/users-catalog/users-list/user-item/UserItem.spec.jsx
+++ client/src/components/pages/catalog-page/users-catalog/users-list/user-item/UserItem.spec.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, prettyDOM, render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
 import { describe, expect, it, vi, beforeEach } from "vitest";
 
 import { UserContext } from "../../../../../../contexts/user-context";
@@ -10,9 +10,9 @@ vi.mock("./user-item-details/UserItemDetails", () => ({
 }));
 
 vi.mock("./add-friend-button/AddFriendButton", () => ({
-    default: ({ isAddedAsFriend, handleAddFriendClick, handleUnfriendClick }) => <div data-testid="add-friend-button-comp">
+    default: ({ isAddedAsFriend, handleAddFriendClick, handleRemoveFriend }) => <div data-testid="add-friend-button-comp">
         {isAddedAsFriend ? (
-            <button data-testid="unfriend" onClick={handleUnfriendClick}></button>
+            <button data-testid="unfriend" onClick={handleRemoveFriend}></button>
         ) : (
             <button data-testid="add-friend" onClick={handleAddFriendClick}></button>
         )}
@@ -27,8 +27,8 @@ const user = {
     age: "28",
 };
 
-const handleAddFriendMock = vi.fn().mockResolvedValue(true);
-const handleRemoveFriend = vi.fn().mockResolvedValue(true);
+    const handleAddFriendMock = vi.fn();
+    const handleRemoveFriendMock = vi.fn();
 
 function setup(options = {
     isAddedAsFriend: false,
@@ -43,12 +43,17 @@ function setup(options = {
         <UserContext.Provider value={{ isUser: options.isUserValue }}>
             <UserItem
                 handleAddFriend={handleAddFriendMock}
-                handleRemoveFriend={handleRemoveFriend}
+                handleRemoveFriend={handleRemoveFriendMock}
                 user={userWithFriends}
             />
         </UserContext.Provider>
     );
 };
+
+beforeEach(() => {
+    handleAddFriendMock.mockResolvedValue(true);
+    handleRemoveFriendMock.mockResolvedValue(true);
+})
 
 describe("UserItem component", () => {
     it("renders UserItenDeails with passed props", () => {
@@ -90,5 +95,19 @@ describe("UserItem component", () => {
             expect(screen.queryByTestId("unfriend")).not.toBeInTheDocument();
             expect(screen.getByTestId("add-friend")).toBeInTheDocument();
         }
+    });
+
+    it("triggers handleAddFriend and changes setIsAddedAsFriend to true on successfull call", async () => {
+        setup();
+
+        fireEvent.click(screen.getByTestId("add-friend"));
+        screen.debug()
+
+        await waitFor(() => {
+            expect(handleAddFriendMock).toHaveBeenCalledWith(user);
+        });
+
+        expect(screen.queryByTestId("add-friend")).not.toBeInTheDocument();
+        expect(screen.getByTestId("unfriend")).toBeInTheDocument();
     });
 });
