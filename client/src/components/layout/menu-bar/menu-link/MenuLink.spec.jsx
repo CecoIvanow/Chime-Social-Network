6ca7e492e3e282819c69f28f2797d4fe34a97b7d
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-23 23:05:34 +0200

    client/test: add image redner test on passed linkImageUri for MenuLink component

--- client/src/components/layout/menu-bar/menu-link/MenuLink.spec.jsx
+++ client/src/components/layout/menu-bar/menu-link/MenuLink.spec.jsx
@@ -6,7 +6,10 @@ import { MemoryRouter } from "react-router";
 import MenuLink from "./MenuLink";
 
 const LINK_URL = "/";
-const LINK_TITLE = "Home"
+const LINK_TITLE = "Home";
+const IMAGE_URI = "image.png";
+const IMAGE_ALT = "image";
+const LINK_TEXT = "Test123";
 
 describe("MenuLink component", () => {
     it("renders component with passed props", () => {
@@ -21,5 +24,21 @@ describe("MenuLink component", () => {
 
         expect(screen.getByRole("link")).toHaveAttribute("href", LINK_URL);
         expect(screen.getByRole("link")).toHaveAttribute("title", LINK_TITLE);
+    });
+
+    it("renders an image on passed linkImageUri", () => {
+        render(
+            <MemoryRouter>
+                <MenuLink
+                    linkImageUri={IMAGE_URI}
+                    linkImageAlt={IMAGE_ALT}
+                />
+            </MemoryRouter>
+        );
+
+        expect(screen.getByRole("img")).toHaveAttribute("src", IMAGE_URI);
+        expect(screen.getByRole("img")).toHaveAttribute("alt", IMAGE_ALT);
+
+        expect(screen.getByRole("link")).not.toHaveTextContent(LINK_TEXT);
     });
 });
