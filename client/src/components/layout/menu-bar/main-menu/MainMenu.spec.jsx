257deb117b84ca095c2fc711424885a408c8157f
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-24 13:55:03 +0200

    client/test: add parameterized conditional rendering tests for Profile navigational link in MainMenu

--- client/src/components/layout/menu-bar/main-menu/MainMenu.spec.jsx
+++ client/src/components/layout/menu-bar/main-menu/MainMenu.spec.jsx
@@ -18,9 +18,9 @@ vi.mock("../menu-link/MenuLink", () => ({
 const isUserMock = "userId";
 
 function setup(options = {
-    isUserIsEmpty: false
+    isUserValid: false
 }) {
-    const isUser = options.isUserIsEmpty ? "" : isUserMock;
+    const isUser = options.isUserValid ? isUserMock : "";
 
     render(
         <MemoryRouter>
@@ -32,11 +32,11 @@ function setup(options = {
 };
 
 describe("MainMenu component", () => {
-    it("renders Home and Catalog MenuLink child components with passed props", () => {
+    it("renders Home and Catalog MenuLink navigational components with passed props", () => {
         setup();
 
         const HomeLinkEl = screen.getByRole("link", { name: "Home" });
-        const HomeImgEl = screen.getByRole("img", { name: "Home"});
+        const HomeImgEl = screen.getByRole("img", { name: "Home" });
 
         expect(HomeLinkEl).toHaveAttribute("href", "/");
         expect(HomeLinkEl).toHaveAttribute("title", "Home");
@@ -52,5 +52,29 @@ describe("MainMenu component", () => {
 
         expect(catalogImgEl).toHaveAttribute("src", "\\images\\catalog-icon.png");
         expect(catalogImgEl).toHaveAttribute("alt", "Catalog");
+    });
+
+    it.each([
+        { name: "renders Profile navigational component on valid isUser", isUserValid: true },
+        { name: "does not render Profile navigational component on empty isUser", isUserValid: false }
+    ])("$name", ({ isUserValid }) => {
+        setup({
+            isUserValid
+        });
+
+        
+        if (isUserValid) {
+            const profileLinkEl = screen.getByRole("link", { name: "Profile" });
+            const profileImgEl = screen.getByRole("img", { name: "Profile" });
+
+            expect(profileLinkEl).toHaveAttribute("href", `/profile/${isUserMock}`);
+            expect(profileLinkEl).toHaveAttribute("title", "Profile");
+
+            expect(profileImgEl).toHaveAttribute("src", "\\images\\profile-icon.png");
+            expect(profileImgEl).toHaveAttribute("alt", "Profile");
+        } else {
+            expect(screen.queryByRole("link", {name: "Profile"})).not.toBeInTheDocument();
+            expect(screen.queryByRole("img", {name: "Profile"})).not.toBeInTheDocument();
+        }
     });
 });
