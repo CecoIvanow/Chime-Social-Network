6a93868691951139cc3f72207078c158e28277ea
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-26 01:06:02 +0200

    client/test/refactor: change single to double quotes for consistency

--- client/src/components/ui/auth/auth-button/AuthButton.spec.jsx
+++ client/src/components/ui/auth/auth-button/AuthButton.spec.jsx
@@ -1,31 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 
-import AuthButton from './AuthButton';
+import AuthButton from "./AuthButton";
 
-describe('AuthButton component', () => {
-    it('renders a submit button with the correct text and container', () => {
+describe("AuthButton component", () => {
+    it("renders a submit button with the correct text and container", () => {
         render(<AuthButton
-            buttonText='Login'
+            buttonText="Login"
         />);
 
-        expect(screen.getByRole('button')).toHaveValue('Login');
+        expect(screen.getByRole("button")).toHaveValue("Login");
     });
 
-    it('isPending is false by default', () => {
+    it("isPending is false by default", () => {
         render(<AuthButton
-            buttonText='Login'
+            buttonText="Login"
         />);
 
-        expect(screen.getByRole('button')).not.toBeDisabled();
-    })
+        expect(screen.getByRole("button")).not.toBeDisabled();
+    });
 
-    it('is disabled if isPending is true', () => {
+    it("is disabled if isPending is true", () => {
         render(<AuthButton
-            buttonText='Loading...'
+            buttonText="Loading..."
             isPending={true}
         />);
 
-        expect(screen.getByRole('button')).toBeDisabled();
-    })
-})
+        expect(screen.getByRole("button")).toBeDisabled();
+    });
+});
