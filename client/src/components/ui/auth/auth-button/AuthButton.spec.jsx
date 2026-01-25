f3ac5908e237149a99592442f6d3586e788e1207
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-26 01:19:09 +0200

    client/test/refactor: extract AuthButton render logic in separate function wrapper

--- client/src/components/ui/auth/auth-button/AuthButton.spec.jsx
+++ client/src/components/ui/auth/auth-button/AuthButton.spec.jsx
@@ -3,28 +3,33 @@ import { describe, expect, it } from "vitest";
 
 import AuthButton from "./AuthButton";
 
+const buttonText = "Login";
+
+function setup(options={
+    isPending: false,
+}) {
+    render(
+        <AuthButton buttonText={buttonText} isPending={options.isPending} />
+    );
+};
+
 describe("AuthButton component", () => {
     it("renders a submit button with the correct text and container", () => {
-        render(<AuthButton
-            buttonText="Login"
-        />);
+        setup();
 
-        expect(screen.getByRole("button")).toHaveValue("Login");
+        expect(screen.getByRole("button")).toHaveValue(buttonText);
     });
 
     it("isPending is false by default", () => {
-        render(<AuthButton
-            buttonText="Login"
-        />);
+        setup();
 
         expect(screen.getByRole("button")).not.toBeDisabled();
     });
 
     it("is disabled if isPending is true", () => {
-        render(<AuthButton
-            buttonText="Loading..."
-            isPending={true}
-        />);
+        setup({
+            isPending: true
+        });
 
         expect(screen.getByRole("button")).toBeDisabled();
     });
