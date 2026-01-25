7963bca6aaead4d45164c640717222e623f61ab6
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-25 13:04:51 +0200

    client/refactor: extract render into a function wrapper

--- client/src/components/ui/buttons/button/Button.spec.jsx
+++ client/src/components/ui/buttons/button/Button.spec.jsx
@@ -10,16 +10,30 @@ const buttonProps = {
     clickHandler: vi.fn(),
 }
 
-beforeEach(() => render(<Button onClickHandler={buttonProps.clickHandler} buttonName={buttonProps.content} />));
+function setup(options={
+    hasTextContent: true
+}) {
+    const content = options.hasTextContent ?
+    buttonProps.content :
+    null;
+
+    render(
+        <Button onClickHandler={buttonProps.clickHandler} buttonName={content} />
+    );
+}
 
 describe('Button component', () => {
     it('renders button with passed text label', () => {
+        setup();
+
         expect(screen.getByRole('button')).toHaveTextContent(buttonProps.content);
     });
 
     it('triggers click handler on click', async () => {
-        const buttonElement = screen.getByRole('button');
+        setup();
+        
         const user = userEvent.setup();
+        const buttonElement = screen.getByRole('button');
         
         user.click(buttonElement);
 
