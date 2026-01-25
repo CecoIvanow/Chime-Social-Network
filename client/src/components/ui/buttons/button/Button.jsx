7659a7d7fa093976e55389e0fbb55f31f6efa8ce
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-26 01:11:08 +0200

    client/refactor: replace react fragment with parantheses on Button return for consistency

--- client/src/components/ui/buttons/button/Button.jsx
+++ client/src/components/ui/buttons/button/Button.jsx
@@ -6,12 +6,12 @@ export default function Button({
     const label = buttonName ?? "";
     const style = btnStyle ?? "";
 
-    return <>
+    return (
         <button
             onClick={onClickHandler}
             className={style}
         >
             {label}
         </button>
-    </>
+    )
 }
