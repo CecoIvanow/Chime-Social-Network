e3856f9c9d54c3cce0e5e93aaf968f8361433cde
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-25 16:12:00 +0200

    client/test/refactor: remove manual cleanup and set vite test globals to true

--- client/src/tests/setup.js
+++ client/src/tests/setup.js
@@ -1,9 +1,7 @@
 import { beforeEach, vi } from "vitest"
-import { cleanup } from "@testing-library/react"
 import "@testing-library/jest-dom/vitest"
 
 beforeEach(() => {
-    cleanup();
     vi.resetAllMocks();
     vi.clearAllTimers();
 })
