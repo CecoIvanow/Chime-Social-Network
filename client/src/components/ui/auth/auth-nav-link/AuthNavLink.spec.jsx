fc146ed6b02786803bcf119a86d72e3e67eec743
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-26 22:58:43 +0200

    client/test/refator: extract AuthNavLink render in beforeEach callback func

--- client/src/components/ui/auth/auth-nav-link/AuthNavLink.spec.jsx
+++ client/src/components/ui/auth/auth-nav-link/AuthNavLink.spec.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, beforeEach } from "vitest";
 
 import AuthNavLink from "./AuthNavLink";
 import { MemoryRouter } from "react-router";
@@ -7,18 +7,21 @@ import { MemoryRouter } from "react-router";
 const authNavLinkProps = {
     buttonLabel: "Don't have an account?",
     path: "/register",
-}
+};
+
+beforeEach(() => {
+    render(
+        <MemoryRouter>
+            <AuthNavLink
+                path={authNavLinkProps.path}
+                buttonText={authNavLinkProps.buttonLabel}
+            />
+        </MemoryRouter>
+    );
+});
 
 describe('AuthNavLink component', () => {
     it('renders with container and Link', () => {
-        render(
-            <MemoryRouter>
-                <AuthNavLink
-                    buttonText={authNavLinkProps.buttonLabel}
-                />
-            </MemoryRouter>
-        );
-
         const container = screen.getByTestId('to-auth-container');
         const link = screen.getByText(authNavLinkProps.buttonLabel);
 
@@ -29,14 +32,6 @@ describe('AuthNavLink component', () => {
     })
 
     it('ensures Link is a child of the container', () => {
-        render(
-            <MemoryRouter>
-                <AuthNavLink
-                    buttonText={authNavLinkProps.buttonLabel}
-                />
-            </MemoryRouter>
-        );
-
         const container = screen.getByTestId('to-auth-container');
         const link = screen.getByText(authNavLinkProps.buttonLabel);
 
@@ -47,15 +42,6 @@ describe('AuthNavLink component', () => {
     })
 
     it('renders with passed props', () => {
-        render(
-            <MemoryRouter>
-                <AuthNavLink
-                    path={authNavLinkProps.path}
-                    buttonText={authNavLinkProps.buttonLabel}
-                />
-            </MemoryRouter>
-        );
-
         const link = screen.getByText(authNavLinkProps.buttonLabel);
 
         expect(link).toHaveTextContent(authNavLinkProps.buttonLabel);
