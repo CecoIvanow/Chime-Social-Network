00be024286ce1296d47f92e712bb369679a0a909
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-26 23:34:18 +0200

    client/test/refator: extract CreateContentInput render logic in function wrapper

--- client/src/components/ui/create-content-input/CreateContentInput.spec.jsx
+++ client/src/components/ui/create-content-input/CreateContentInput.spec.jsx
@@ -3,13 +3,27 @@ import { describe, expect, it, vi } from "vitest";
 
 import CreateContentInput from "./CreateContentInput";
 
-const createContentInputProps = {
-    text: "Testing text!"
-}
+const onTextChangeMock = vi.fn();
+
+function setup(options = {
+    hasPlaceholderTextProp: true,
+}) {
+    const placeholderText = options.hasPlaceholderTextProp ? "Post your comment..." : null;
+
+    render(
+        <CreateContentInput
+            onTextChangeHandler={onTextChangeMock}
+            placeholderText={placeholderText}
+            text={'Testing text!'}
+        />
+    );
+};
 
 describe('CreateContentInput', () => {
     it("links label and input correctly via htmlFor and id", () => {
-        render(<CreateContentInput />)
+        setup({
+            hasPlaceholderTextProp: false
+        });
 
         const label = screen.getByTestId('entry-label');
         const textarea = screen.getByPlaceholderText('Share your thoughts...');
@@ -19,19 +33,18 @@ describe('CreateContentInput', () => {
     });
 
     it('renders input with correct default value and place holder text', () => {
-        render(<CreateContentInput
-            placeholderText={'Post your comment'}
-            text={'Testing text!'}
-        />);
+        setup();
 
-        const input = screen.getByPlaceholderText('Post your comment');
+        const input = screen.getByPlaceholderText('Post your comment...');
 
         expect(input).toHaveValue('Testing text!');
         expect(input).toBeInTheDocument();
     });
 
     it('renders input with default name and type attributes', () => {
-        render(<CreateContentInput />);
+        setup({
+            hasPlaceholderTextProp: false
+        });
 
         const input = screen.getByPlaceholderText('Share your thoughts...');
 
@@ -40,17 +53,15 @@ describe('CreateContentInput', () => {
     });
 
     it('on text change handler gets called on change', () => {
-        const onTextChangeMock = vi.fn();
-
-        render(<CreateContentInput 
-            onTextChangeHandler={onTextChangeMock}
-        />)
+        setup({
+            hasPlaceholderTextProp: false
+        });
 
         const input = screen.getByPlaceholderText("Share your thoughts...");
 
         expect(onTextChangeMock).not.toHaveBeenCalled();
 
-        fireEvent.change(input, { target: {value: 'Unit test'} });
+        fireEvent.change(input, { target: { value: 'Unit test' } });
         expect(onTextChangeMock).toHaveBeenCalledTimes(1);
     });
 });
