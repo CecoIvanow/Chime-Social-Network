3e775c598077c469ef168fc8504e553b3f54faa7
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-27 20:24:58 +0200

    client/test/refactor: extract SectionHeading header label in a reusable variable

--- client/src/components/ui/headings/SectionHeading.spec.jsx
+++ client/src/components/ui/headings/SectionHeading.spec.jsx
@@ -3,10 +3,12 @@ import { describe, expect, it, beforeEach } from "vitest";
 
 import SectionHeading from "./SectionHeading";
 
-beforeEach(() => render(<SectionHeading sectionName={"Friends"} />))
+const headerLabel = "Friends";
+
+beforeEach(() => render(<SectionHeading sectionName={headerLabel} />))
 
 describe("SectionHeading component", () => {
     it("Should render sectionName text", () => {
-        expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent("Friends");
+        expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(headerLabel);
     });
 });
