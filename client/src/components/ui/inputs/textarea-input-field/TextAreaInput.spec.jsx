7e0ea64e666fc3a4c853a1e2a1a16146b277df60
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-27 21:53:46 +0200

    client/test/refactor: remove redundant TextAreaInput tests and data-testid attrbute

--- client/src/components/ui/inputs/textarea-input-field/TextAreaInput.spec.jsx
+++ client/src/components/ui/inputs/textarea-input-field/TextAreaInput.spec.jsx
@@ -4,21 +4,6 @@ import { describe, expect, it } from "vitest";
 import TextAreaInput from "./TextAreaInput";
 
 describe('TextAreaInput component', () => {
-    it('renders form-group container, input and textarea', () => {
-        render(<TextAreaInput
-            fieldName={'Biography'}
-            inputName={'bio'}
-        />);
-
-        const formFroup = screen.getByTestId('form-group');
-        const label = screen.getByText('Biography');
-        const textarea = screen.getByLabelText('Biography');
-
-        expect(formFroup).toBeInTheDocument();
-        expect(label).toBeInTheDocument();
-        expect(textarea).toBeInTheDocument();
-    })
-
     it("links label and textarea correctly via htmlFor, id, and name attributes", () => {
         render(<TextAreaInput
             fieldName='Bio'
@@ -31,7 +16,7 @@ describe('TextAreaInput component', () => {
         expect(label).toHaveAttribute('for', 'bio');
         expect(textarea).toHaveAttribute('id', 'bio');
         expect(textarea).toHaveAttribute('name', 'bio');
-    })
+    });
 
     it('renders textarea with default value', () => {
         render(<TextAreaInput
@@ -42,5 +27,5 @@ describe('TextAreaInput component', () => {
 
         const textarea = screen.getByLabelText('Bio');
         expect(textarea).toHaveValue('Hello!');
-    })
-})
+    });
+});
