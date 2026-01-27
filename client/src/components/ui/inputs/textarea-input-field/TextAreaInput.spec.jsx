d157e0a31f8a042e1305a36797992eb7e1047ae4
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-27 22:07:26 +0200

    client/test/refactor: extract TextAreaInput props in a reusable object

--- client/src/components/ui/inputs/textarea-input-field/TextAreaInput.spec.jsx
+++ client/src/components/ui/inputs/textarea-input-field/TextAreaInput.spec.jsx
@@ -3,28 +3,34 @@ import { describe, expect, it, beforeEach } from "vitest";
 
 import TextAreaInput from "./TextAreaInput";
 
+const props = {
+    fieldName: "Bio",
+    initialValue: 'Hello!',
+    inputName: "bio",
+};
+
 beforeEach(() => {
     render(
         <TextAreaInput
-            inputName={'bio'}
-            fieldName={'Bio'}
-            initialValue={'Hello!'}
+            fieldName={props.fieldName}
+            initialValue={props.initialValue}
+            inputName={props.inputName}
         />
     );
 });
 
 describe('TextAreaInput component', () => {
     it("links label and textarea correctly via htmlFor and id attributes", () => {
-        const label = screen.getByText('Bio');
-        const textarea = screen.getByLabelText('Bio');
+        const label = screen.getByText(props.fieldName);
+        const textarea = screen.getByLabelText(props.fieldName);
 
-        expect(label).toHaveAttribute('for', 'bio');
+        expect(label).toHaveAttribute('for', props.inputName);
         expect(textarea).toHaveAttribute('id', 'bio');
         expect(textarea).toHaveAttribute('name', 'bio');
     });
 
     it('renders textarea with correct value and name attributes', () => {
-        const textarea = screen.getByLabelText('Bio');
-        expect(textarea).toHaveValue('Hello!');
+        const textarea = screen.getByLabelText(props.fieldName);
+        expect(textarea).toHaveValue(props.initialValue);
     });
 });
