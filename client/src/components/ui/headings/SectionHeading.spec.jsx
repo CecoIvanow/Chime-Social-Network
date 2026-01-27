f6e49beeeba3735e4d9c3415803f67718fe6031c
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-27 20:21:03 +0200

    client/test/refactor: extract SectionHeading render to beforeEach

--- client/src/components/ui/headings/SectionHeading.spec.jsx
+++ client/src/components/ui/headings/SectionHeading.spec.jsx
@@ -1,14 +1,12 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, beforeEach } from "vitest";
 
 import SectionHeading from "./SectionHeading";
 
+beforeEach(() => render(<SectionHeading sectionName={'Friends'} />))
+
 describe('SectionHeading component', () => {
     it('Should render sectionName text', () => {
-        
-
-        render(<SectionHeading sectionName={'Friends'}/>);
-
         expect(screen.getByRole('heading')).toHaveTextContent('Friends');
     });
 });
