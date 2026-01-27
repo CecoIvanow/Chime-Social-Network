d9687d12e68d02a6fe7a09444da0f16cbfbd9eb8
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-27 20:23:16 +0200

    client/test/refactor: replace InputField single with double quotes for consistency

--- client/src/components/ui/inputs/input-field/InputField.spec.jsx
+++ client/src/components/ui/inputs/input-field/InputField.spec.jsx
@@ -2,16 +2,16 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import InputField from "./InputField";
 
-describe('InputField component', () => {
-    it('renders with form-group container, label and input', () => {
+describe("InputField component", () => {
+    it("renders with form-group container, label and input", () => {
         render(<InputField
-            fieldName='Age'
-            inputName='age'
+            fieldName="Age"
+            inputName="age"
         />)
 
-        const formGroup = screen.getByTestId('form-group');
-        const label = screen.getByText('Age');
-        const textarea = screen.getByLabelText('Age');
+        const formGroup = screen.getByTestId("form-group");
+        const label = screen.getByText("Age");
+        const textarea = screen.getByLabelText("Age");
 
         expect(formGroup).toBeInTheDocument();
         expect(label).toBeInTheDocument();
@@ -20,29 +20,29 @@ describe('InputField component', () => {
 
     it("links label and input correctly via htmlFor, id, and name attributes", () => {
         render(<InputField
-            fieldName='Age'
-            inputName='age'
+            fieldName="Age"
+            inputName="age"
         />)
 
-        const label = screen.getByText('Age');
-        const textarea = screen.getByLabelText('Age');
+        const label = screen.getByText("Age");
+        const textarea = screen.getByLabelText("Age");
 
-        expect(label).toHaveAttribute('for', 'age');
-        expect(textarea).toHaveAttribute('id', 'age');
-        expect(textarea).toHaveAttribute('name', 'age');
+        expect(label).toHaveAttribute("for", "age");
+        expect(textarea).toHaveAttribute("id", "age");
+        expect(textarea).toHaveAttribute("name", "age");
     })
 
-    it('renders textarea with default value and type', () => {
+    it("renders textarea with default value and type", () => {
         render(<InputField
-            fieldName='Age'
-            inputName='age'
-            initialValue='27'
-            inputType='string'
+            fieldName="Age"
+            inputName="age"
+            initialValue="27"
+            inputType="string"
         />)
 
-        const textarea = screen.getByLabelText('Age');
+        const textarea = screen.getByLabelText("Age");
 
-        expect(textarea).toHaveValue('27');
-        expect(textarea).toHaveAttribute('type', 'string');
+        expect(textarea).toHaveValue("27");
+        expect(textarea).toHaveAttribute("type", "string");
     })
 })
