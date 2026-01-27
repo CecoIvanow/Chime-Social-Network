102fb75690d9dedd6c78956fb05fcb7aae04ba85
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-27 20:35:26 +0200

    client/test/refactor: extract InputField  render to beforeEach

--- client/src/components/ui/inputs/input-field/InputField.spec.jsx
+++ client/src/components/ui/inputs/input-field/InputField.spec.jsx
@@ -1,34 +1,33 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, beforeEach } from "vitest";
 
 import InputField from "./InputField";
 
-describe("InputField component", () => {
-    it("links label and input correctly via htmlFor, id, and name attributes", () => {
-        render(<InputField
+beforeEach(() => {
+    render(
+        <InputField
             fieldName="Age"
             inputName="age"
-        />)
+            initialValue="27"
+            inputType="string"
+        />
+    );
+});
 
+describe("InputField component", () => {
+    it("links label and input correctly via htmlFor, id, and name attributes", () => {
         const label = screen.getByText("Age");
         const textarea = screen.getByLabelText("Age");
 
         expect(label).toHaveAttribute("for", "age");
         expect(textarea).toHaveAttribute("id", "age");
         expect(textarea).toHaveAttribute("name", "age");
-    })
+    });
 
     it("renders textarea with default value and type", () => {
-        render(<InputField
-            fieldName="Age"
-            inputName="age"
-            initialValue="27"
-            inputType="string"
-        />)
-
         const textarea = screen.getByLabelText("Age");
 
         expect(textarea).toHaveValue("27");
         expect(textarea).toHaveAttribute("type", "string");
-    })
-})
+    });
+});
