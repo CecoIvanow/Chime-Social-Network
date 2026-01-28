9b7df78fa0b925d1523c58ac4abbdeca98cfc379
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-28 23:59:05 +0200

    client/test/refactor: remove redundant AuthHeaderTitle test

--- client/src/components/shared/auth/auth-header-title/AuthHeaderTitle.spec.jsx
+++ client/src/components/shared/auth/auth-header-title/AuthHeaderTitle.spec.jsx
@@ -12,11 +12,6 @@ beforeEach(() => {
 });
 
 describe("AuthHeaderTitle component", () => {
-    it("renders on screen", () => {
-
-        expect(screen.getByTestId("auth-header-title")).toBeInTheDocument();
-    });
-
     it("renders with passed text content", () => {
 
         expect(screen.getByText("Password")).toHaveTextContent("Password");
