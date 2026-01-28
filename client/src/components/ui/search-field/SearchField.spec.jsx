1bbdcfe28a1766482220ff15218975e8655609ba
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-28 19:19:46 +0200

    client/test/refactor: extract SearchField props in reusable object

--- client/src/components/ui/search-field/SearchField.spec.jsx
+++ client/src/components/ui/search-field/SearchField.spec.jsx
@@ -3,13 +3,17 @@ import { describe, expect, it, vi, beforeEach } from "vitest";
 
 import SearchField from "./SearchField";
 
-const mockSearchParams = vi.fn();
+const mockProps = {
+    setSearchParams: vi.fn(),
+    searchBy: "content",
+}
 
 beforeEach(() => {
+    vi.useFakeTimers();
+
     render(
         <SearchField
-            setSearchParams={mockSearchParams}
-            searchBy="content"
+            {...mockProps}
         />
     );
 });
@@ -25,22 +29,18 @@ describe("SearchField component", () => {
     });
 
     it("Should set correct search params after 1250 ms on change", () => {
-        vi.useFakeTimers();
-
         const input = screen.getByTestId("search-field-input");
 
         fireEvent.change(input, { target: { value: "React" } });
 
         vi.advanceTimersByTime(1249);
-        expect(mockSearchParams).toBeCalledTimes(0);
+        expect(mockProps.setSearchParams).toBeCalledTimes(0);
 
         vi.advanceTimersByTime(1);
-        expect(mockSearchParams).toBeCalledWith("React");
+        expect(mockProps.setSearchParams).toBeCalledWith("React");
     });
 
     it("Should set correct search params after multiple changes", () => {
-        vi.useFakeTimers();
-
         const input = screen.getByTestId("search-field-input");
 
         fireEvent.change(input, { target: { value: "R" } });
@@ -51,11 +51,11 @@ describe("SearchField component", () => {
 
         fireEvent.change(input, { target: { value: "React" } });
         vi.advanceTimersByTime(1249);
-        expect(mockSearchParams).not.toBeCalledWith("React");
+        expect(mockProps.setSearchParams).not.toBeCalledWith("React");
 
         fireEvent.change(input, { target: { value: "Re" } });
         vi.advanceTimersByTime(1250);
-        expect(mockSearchParams).toBeCalledTimes(1);
-        expect(mockSearchParams).toBeCalledWith("Re");
+        expect(mockProps.setSearchParams).toBeCalledTimes(1);
+        expect(mockProps.setSearchParams).toBeCalledWith("Re");
     })
 })
