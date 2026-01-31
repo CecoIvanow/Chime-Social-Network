b6b92f3ab9c913ad076b221ff8ff9f4195825cb0
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-01-31 23:33:05 +0200

    client/test/refactor: extract PostCommentButton render in a beforeEach

--- client/src/components/shared/post/post-interactions/post-buttons/post-interaction-buttons/post-comment-button/PostCommentButton.spec.jsx
+++ client/src/components/shared/post/post-interactions/post-buttons/post-interaction-buttons/post-comment-button/PostCommentButton.spec.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it, vi, beforeEach } from "vitest";
 
 import { PostContext } from "../../../../../../../contexts/post-context";
 
@@ -7,7 +7,7 @@ import PostCommentButton from "./PostCommentButton";
 
 const post = {
     _id: 1
-}
+};
 
 vi.mock("../../../../../../ui/buttons/link-button/LinkButton", () => ({
     default: ({ urlLink }) => (
@@ -15,15 +15,16 @@ vi.mock("../../../../../../ui/buttons/link-button/LinkButton", () => ({
     )
 }));
 
+beforeEach(() => {
+    render(
+        <PostContext.Provider value={{ post }}>
+            <PostCommentButton />
+        </PostContext.Provider >
+    );
+});
+
 describe("PostCommentButton component", () => {
     it("renders LinkButton with correct urlLink prop", () => {
-        render(
-            <PostContext.Provider value={{ post }}>
-                <PostCommentButton />
-            </PostContext.Provider >
-        );
-
         expect(screen.getByTestId('urlLink')).toHaveTextContent(`/post/${post._id}/details`);
     });
-
-})
+});
