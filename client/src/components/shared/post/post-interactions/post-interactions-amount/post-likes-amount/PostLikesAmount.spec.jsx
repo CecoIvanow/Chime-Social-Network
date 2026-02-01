a2fe9a8e127787b48c83b5a2ddd9d1adf9619545
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-01 15:07:38 +0200

    client/test/refactor: extract PostLikesAmount render in a beforeEach

--- client/src/components/shared/post/post-interactions/post-interactions-amount/post-likes-amount/PostLikesAmount.spec.jsx
+++ client/src/components/shared/post/post-interactions/post-interactions-amount/post-likes-amount/PostLikesAmount.spec.jsx
@@ -1,20 +1,22 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, beforeEach } from "vitest";
 
 import { LikesContext } from "../../../../../../contexts/likes-context";
 
 import PostLikesAmount from "./PostLikesAmount";
 
+const likes = ["likeOne", "likeTwo"];
+
+beforeEach(() => {
+    render(
+        <LikesContext.Provider value={{ likes }}>
+            <PostLikesAmount />
+        </LikesContext.Provider>
+    );
+});
+
 describe("PostLikesAmount component", () => {
-    const likes = ["likeOne", "likeTwo"];
-
     it("renders component with correct amount of likes", () => {
-        render(
-            <LikesContext.Provider value={{ likes }}>
-                <PostLikesAmount />
-            </LikesContext.Provider>
-        );
-
         expect(screen.getByText(`Likes: ${likes.length}`)).toBeInTheDocument();
     });
 });
