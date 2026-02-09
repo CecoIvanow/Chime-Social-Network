2fce80d93df87d8079035d7693d2e1dd6a2f30d6
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-09 22:31:40 +0200

    client/test/refactor: remove redundant imports in PostsCatalog

--- client/src/components/pages/catalog-page/posts-catalog/PostsCatalog.spec.jsx
+++ client/src/components/pages/catalog-page/posts-catalog/PostsCatalog.spec.jsx
@@ -1,5 +1,5 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { describe, expect, it, vi, beforeEach } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
 
 import { TotalPostsContext } from "../../../../contexts/total-posts-context";
 
