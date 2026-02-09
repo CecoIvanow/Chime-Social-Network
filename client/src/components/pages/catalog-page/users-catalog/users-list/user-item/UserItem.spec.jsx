cbb4532c29b1cdd1f96c5b83015d79c229dbe26a
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-10 00:01:59 +0200

    client/test/refactor: remove redundant imports in UserItemDetails

--- client/src/components/pages/catalog-page/users-catalog/users-list/user-item/UserItem.spec.jsx
+++ client/src/components/pages/catalog-page/users-catalog/users-list/user-item/UserItem.spec.jsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { describe, expect, it, vi, beforeEach } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 
 import { UserContext } from "../../../../../../contexts/user-context";
 
