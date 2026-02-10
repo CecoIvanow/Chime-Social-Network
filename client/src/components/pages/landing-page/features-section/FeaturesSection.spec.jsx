b3511c0ab8e163159ce5a812f265e9dadfeb2b76
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-10 22:18:50 +0200

    client/test/refactor: separate render FeaturesSection render tests for test clarity

--- client/src/components/pages/landing-page/features-section/FeaturesSection.spec.jsx
+++ client/src/components/pages/landing-page/features-section/FeaturesSection.spec.jsx
@@ -21,15 +21,17 @@ const featureCards = [
 beforeEach(() => render(<FeaturesSection />));
 
 describe("FeaturesSection component", () => {
-    it("renders components with props", () => {
+    it("renders feature card with title", () => {
+        expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent("Key Features");
+    });
+
+    it("renders correct amount of feature cards with description and title", () => {
         const featureCardsTitles = screen.getAllByTestId("feature-card-title");
         const featureCardsContent = screen.getAllByTestId("feature-card-content");
 
         for (let i = 0; i < featureCards.length; i++) {
             expect(featureCardsTitles[i]).toHaveTextContent(featureCards[i].title);
             expect(featureCardsContent[i]).toHaveTextContent(featureCards[i].description);
-        }
-
-        expect(screen.getByRole("heading", {level: 2})).toHaveTextContent("Key Features");
+        };
     });
 });
