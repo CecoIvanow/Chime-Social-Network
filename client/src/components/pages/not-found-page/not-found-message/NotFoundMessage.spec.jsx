6d08b4c7ced4ae0df25969790132c85fe6797b4f
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-11 22:04:48 +0200

    client/test/refactor: remove redundant imports in NotFoundMessage

--- client/src/components/pages/not-found-page/not-found-message/NotFoundMessage.spec.jsx
+++ client/src/components/pages/not-found-page/not-found-message/NotFoundMessage.spec.jsx
@@ -1,5 +1,5 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 
 import NotFoundMessage from "./NotFoundMessage";
 
