9e698f653e1d6e6d1b778394f61812da0d46c4a5
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-11 22:07:13 +0200

    client/test/refactor: extract NotFoundMessage render in a beforeEach

--- client/src/components/pages/not-found-page/not-found-message/NotFoundMessage.spec.jsx
+++ client/src/components/pages/not-found-page/not-found-message/NotFoundMessage.spec.jsx
@@ -1,17 +1,19 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, beforeEach } from "vitest";
 
 import NotFoundMessage from "./NotFoundMessage";
 
 const H1_CONTENT = "404";
 const H2_CONTENT = "the page you requested could not be found";
 
+beforeEach(() => {
+    render(
+        <NotFoundMessage />
+    );
+});
+
 describe("NotFoundMessage component", () => {
     it("renders component with error/not-found message", () => {
-        render(
-            <NotFoundMessage />
-        );
-
         expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(H1_CONTENT);
         expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(H2_CONTENT);
     });
