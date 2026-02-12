b7cd4c54a78c83ef34d37143bff8842b5f855de7
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-12 20:50:24 +0200

    client/test/refactor: reorganize CommentItemHeader imports for consistency

--- client/src/components/pages/post-details-page/comments-section/comment-items-list/comment-item/comment-item-header/CommentItemHeader.spec.jsx
+++ client/src/components/pages/post-details-page/comments-section/comment-items-list/comment-item/comment-item-header/CommentItemHeader.spec.jsx
@@ -1,10 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect, it, vi, beforeEach } from "vitest";
 import { MemoryRouter } from "react-router";
 
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, beforeEach } from "vitest";
+
 import CommentItemHeader from "./CommentItemHeader";
-
-
 
 const comment = {
     owner: {
