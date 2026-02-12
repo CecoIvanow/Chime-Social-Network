ca680efcc71ae971087714725aa98f4dd2d2275a
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-12 20:27:18 +0200

    client/test/refactor: update owner buttons mock in ComemntButtons

--- client/src/components/pages/post-details-page/comments-section/comment-items-list/comment-item/comment-buttons/CommentButtons.spec.jsx
+++ client/src/components/pages/post-details-page/comments-section/comment-items-list/comment-item/comment-buttons/CommentButtons.spec.jsx
@@ -6,7 +6,7 @@ import { UserContext } from "../../../../../../../contexts/user-context";
 import CommentButtons from "./CommentButtons";
 
 vi.mock("../../../../../../shared/controls/owner-buttons/OwnerButtons", () => ({
-    default: ({ itemId }) => <button data-testid="owner-button">{itemId}</button>
+    default: ({ itemId }) => <div data-testid="owner-buttons">{itemId}</div>
 }));
 
 const comment = {
@@ -32,7 +32,7 @@ describe("CommentButtons component", () => {
     it("renders OwnerButtons with passed props on matchimg isUser and owner id", () => {
         setup();
 
-        expect(screen.getByTestId("owner-button")).toHaveTextContent(comment._id);
+        expect(screen.getByTestId("owner-buttons")).toHaveTextContent(comment._id);
     });
 
     it("does not render OwnerButtons on different isUser and owner id", () => {
@@ -40,6 +40,6 @@ describe("CommentButtons component", () => {
             matchingIds: false
         });
 
-        expect(screen.queryByTestId("owner-button")).not.toBeInTheDocument();
+        expect(screen.queryByTestId("owner-buttons")).not.toBeInTheDocument();
     });
 });
