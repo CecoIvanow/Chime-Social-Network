0391fb65a44c10fe7a994208a2d6a8a4a88d8f4b
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-13 21:39:49 +0200

    client/test/refactor: remove redundant imports in CommentsSectionHeader

--- client/src/components/pages/post-details-page/comments-section/comments-section-header/CommentsSectionHeader.spec.jsx
+++ client/src/components/pages/post-details-page/comments-section/comments-section-header/CommentsSectionHeader.spec.jsx
@@ -1,5 +1,5 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, expect, it, vi, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 
 import CommentsSectionHeader from "./CommentsSectionHeader";
 
