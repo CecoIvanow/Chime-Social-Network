e8dcf54040ec5d7d60daaca0116d2ed8c9f0fbd9
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-13 21:55:16 +0200

    client/test/refactor: improve PostEditContent tests to use accessibillity assertions

--- client/src/components/pages/post-details-page/post-edit-content/PostEditContent.spec.jsx
+++ client/src/components/pages/post-details-page/post-edit-content/PostEditContent.spec.jsx
@@ -1,15 +1,13 @@
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it, vi, beforeEach } from "vitest";
 
 import PostEditContent from "./PostEditContent";
-import userEvent from "@testing-library/user-event";
 
 const textChangeHandlerMock = vi.fn();
 
 const PLACEHOLDER_TEXT = "Edit your post content...";
 const POST_TEXT = "Hi, this is my first post!";
-
-let textAreaEl;
 
 beforeEach(() => {
     render(
@@ -18,18 +16,17 @@ beforeEach(() => {
             textChangeHandler={textChangeHandlerMock}
         />
     );
-    textAreaEl = screen.getByPlaceholderText(PLACEHOLDER_TEXT)
 });
 
 describe("PostEditContent", () => {
     it("renders component with passed props", () => {
-        expect(textAreaEl).toHaveValue(POST_TEXT);
+        expect(screen.getByRole("textbox", {value: POST_TEXT})).toHaveAttribute("placeholder", PLACEHOLDER_TEXT);
     });
 
     it("triggers textChangeHandler on input change", async () => {
         const user = userEvent.setup();
 
-        await user.type(textAreaEl, "a");
+        await user.type(screen.getByRole("textbox", { value: POST_TEXT }), "a");
         expect(textChangeHandlerMock).toHaveBeenCalled();
     });
 });
