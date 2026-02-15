fe96586be3408bd4d8d540cea21ac8646fa9c8fe
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-15 11:54:32 +0200

    client/test/refactor: move all mock variables, mock functions and render wrapper outsode of the test suite in ProfileEditPage

--- client/src/components/pages/profile-edit-page/ProfileEditPage.spec.jsx
+++ client/src/components/pages/profile-edit-page/ProfileEditPage.spec.jsx
@@ -103,6 +103,79 @@ vi.mock("react-router", () => ({
 const useParamsMock = vi.fn();
 const navigateMock = vi.fn();
 
+const updateUser = vi.fn();
+const getUserData = vi.fn();
+const abortAll = vi.fn();
+const setAlert = vi.fn();
+const isUser = "curUserId";
+
+ref.mockReturnValue("mock-image-ref");
+
+uploadBytes.mockResolvedValue({
+    ref: "mock-image-ref",
+});
+
+getDownloadURL.mockResolvedValue(
+    "https://firebase.mock/avatar.webp"
+);
+
+const userData = {
+    gender: "Male",
+    imageUrl: "https://www.example.org/example-image.webp",
+    bio: "This is my bio",
+    firstName: "Ivan",
+    lastName: "Petrov",
+    birthday: "1998-03-19",
+    location: "Sofia",
+    occupation: "JS Developer",
+    education: "N/A",
+    status: "Single",
+};
+
+const formProfileInputs = [
+    { fieldName: 'First name', inputType: 'text', inputName: 'firstName', value: userData?.firstName },
+    { fieldName: 'Last name', inputType: 'text', inputName: 'lastName', value: userData?.lastName },
+    { fieldName: 'Birthday', inputType: 'date', inputName: 'birthday', value: userData?.birthday },
+    { fieldName: 'Location', inputType: 'text', inputName: 'location', value: userData?.location },
+    { fieldName: 'Occupation', inputType: 'text', inputName: 'occupation', value: userData?.occupation },
+    { fieldName: 'Education', inputType: 'text', inputName: 'education', value: userData?.education },
+    { fieldName: 'Status', inputType: 'text', inputName: 'status', value: userData?.status },
+];
+
+function renderComp(
+    options = {
+        updateUserResult: true,
+        getUserDataResult: true,
+        useParamsMockValue: isUser,
+    }
+) {
+    useParamsMock.mockReturnValue({ userId: options.useParamsMockValue });
+
+    const updateUserMock = options.updateUserResult ?
+        updateUser.mockResolvedValue(true) :
+        updateUser.mockRejectedValue(new Error("Successfully rejected updateUser!"));
+
+    const getUserDataMock = options.getUserDataResult ?
+        getUserData.mockResolvedValue(userData) :
+        getUserData.mockRejectedValue(new Error("Successfully rejected getUserData!"));
+
+    useUserServices.mockReturnValue({
+        updateUser: updateUserMock,
+        getUserData: getUserDataMock,
+        abortAll,
+    })
+
+    const { unmount } = render(
+        <AlertContext.Provider value={{ setAlert }}>
+            <UserContext.Provider value={{ isUser }}>
+                <ProfileEditPage />
+            </UserContext.Provider>
+        </AlertContext.Provider>
+    );
+
+    return unmount;
+};
+
 beforeEach(() => {
     ref.mockReturnValue("mock-image-ref");
 
@@ -116,79 +189,6 @@ beforeEach(() => {
 });
 
 describe("ProfileEditPage component", () => {
-    const updateUser = vi.fn();
-    const getUserData = vi.fn();
-    const abortAll = vi.fn();
-    const setAlert = vi.fn();
-    const isUser = "curUserId";
-
-    ref.mockReturnValue("mock-image-ref");
-
-    uploadBytes.mockResolvedValue({
-        ref: "mock-image-ref",
-    });
-
-    getDownloadURL.mockResolvedValue(
-        "https://firebase.mock/avatar.webp"
-    );
-
-    const userData = {
-        gender: "Male",
-        imageUrl: "https://www.example.org/example-image.webp",
-        bio: "This is my bio",
-        firstName: "Ivan",
-        lastName: "Petrov",
-        birthday: "1998-03-19",
-        location: "Sofia",
-        occupation: "JS Developer",
-        education: "N/A",
-        status: "Single",
-    };
-
-    const formProfileInputs = [
-        { fieldName: 'First name', inputType: 'text', inputName: 'firstName', value: userData?.firstName },
-        { fieldName: 'Last name', inputType: 'text', inputName: 'lastName', value: userData?.lastName },
-        { fieldName: 'Birthday', inputType: 'date', inputName: 'birthday', value: userData?.birthday },
-        { fieldName: 'Location', inputType: 'text', inputName: 'location', value: userData?.location },
-        { fieldName: 'Occupation', inputType: 'text', inputName: 'occupation', value: userData?.occupation },
-        { fieldName: 'Education', inputType: 'text', inputName: 'education', value: userData?.education },
-        { fieldName: 'Status', inputType: 'text', inputName: 'status', value: userData?.status },
-    ];
-
-    function renderComp(
-        options = {
-            updateUserResult: true,
-            getUserDataResult: true,
-            useParamsMockValue: isUser,
-        }
-    ) {
-        useParamsMock.mockReturnValue({ userId: options.useParamsMockValue });
-
-        const updateUserMock = options.updateUserResult ?
-            updateUser.mockResolvedValue(true) :
-            updateUser.mockRejectedValue(new Error("Successfully rejected updateUser!"));
-
-        const getUserDataMock = options.getUserDataResult ?
-            getUserData.mockResolvedValue(userData) :
-            getUserData.mockRejectedValue(new Error("Successfully rejected getUserData!"));
-
-        useUserServices.mockReturnValue({
-            updateUser: updateUserMock,
-            getUserData: getUserDataMock,
-            abortAll,
-        })
-
-        const { unmount } = render(
-            <AlertContext.Provider value={{ setAlert }}>
-                <UserContext.Provider value={{ isUser }}>
-                    <ProfileEditPage />
-                </UserContext.Provider>
-            </AlertContext.Provider>
-        );
-
-        return unmount;
-    }
-
     it("renders SectionHeading with props", () => {
         const pattern = /^Edit Profile:$/
 
