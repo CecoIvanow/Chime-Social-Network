eac47f412fbf627a9f7784fcfe757678b74d131e
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-16 21:15:18 +0200

    client/test/refactor: remove redundant imports in FriendsList

--- client/src/components/pages/user-home-page/friends-section/friends-list/FriendsList.spec.jsx
+++ client/src/components/pages/user-home-page/friends-section/friends-list/FriendsList.spec.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, getAllByTestId, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 
 import FriendsList from "./FriendsList";
