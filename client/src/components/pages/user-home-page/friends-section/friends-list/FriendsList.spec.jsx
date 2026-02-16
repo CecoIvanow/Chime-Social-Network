ce5a695358f26aa7378c7f2e8e047cb8a3bb547d
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-16 21:24:13 +0200

    client/test/refactor: extract FriendsList props in a reusable object

--- client/src/components/pages/user-home-page/friends-section/friends-list/FriendsList.spec.jsx
+++ client/src/components/pages/user-home-page/friends-section/friends-list/FriendsList.spec.jsx
@@ -7,22 +7,27 @@ vi.mock("./friend-item/FriendItem", () => ({
     default: ({ friend }) => <div data-testid="friend-item">{friend}</div>
 }));
 
-const matchingFriends = ["friend1", "friend2"];
+const mockProps = {
+    matchingFriends: [
+        "friend1",
+        "friend2"
+    ]
+};
 
 beforeEach(() => {
     render(
         <FriendsList
-            matchingFriends={matchingFriends}
+            {...mockProps}
         />
     );
 });
 
 describe("FriendsList component", () => {
     it("renders the correct number of FriendItem components", () => {
-        expect(screen.getAllByTestId("friend-item")).toHaveLength(matchingFriends.length);
+        expect(screen.getAllByTestId("friend-item")).toHaveLength(mockProps.matchingFriends.length);
     });
 
     it("renders FriendItem with passed props", () => {
-        expect(screen.getAllByTestId("friend-item").at(0)).toHaveTextContent(matchingFriends[0]);
+        expect(screen.getAllByTestId("friend-item").at(0)).toHaveTextContent(mockProps.matchingFriends[0]);
     });
 });
