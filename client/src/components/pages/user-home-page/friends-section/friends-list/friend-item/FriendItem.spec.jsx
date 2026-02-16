45a88986beeda2c219a56bfa639180b0424befd6
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-16 21:03:41 +0200

    client/test/refactor: extract FriendItem render in a beforeEach

--- client/src/components/pages/user-home-page/friends-section/friends-list/friend-item/FriendItem.spec.jsx
+++ client/src/components/pages/user-home-page/friends-section/friends-list/friend-item/FriendItem.spec.jsx
@@ -1,37 +1,32 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it, vi, beforeEach } from "vitest";
 
 import FriendItem from "./FriendItem";
 
+const friend = {
+    imageUrl: "https://fake-image.com/ThisIsAFakeImageUrl",
+    firstName: "Ivan",
+    lastName: "Ivanov"
+}
+
+const onClickHandler = vi.fn();
+
+beforeEach(() => {
+    render(
+        <FriendItem
+            friend={friend}
+            onClickHandler={onClickHandler}
+        />
+    );
+});
+
 describe("FrientItem component", () => {
-    const friend = {
-        imageUrl: "https://fake-image.com/ThisIsAFakeImageUrl",
-        firstName: "Ivan",
-        lastName: "Ivanov"
-    }
-
-    const onClickHandler = vi.fn();
-
     it("renders FriendItem component with passed props", () => {
-        render(
-            <FriendItem
-                friend={friend}
-                onClickHandler={onClickHandler}
-            />
-        );
-
         expect(screen.getByText(`${friend.firstName} ${friend.lastName}`)).toBeInTheDocument();
         expect(screen.getByAltText("Profile Picture")).toHaveAttribute('src', friend.imageUrl);
     });
 
     it("triggers onClickHandler on click", () => {
-        render(
-            <FriendItem
-                friend={friend}
-                onClickHandler={onClickHandler}
-            />
-        );
-
         fireEvent.click(screen.getByText(`${friend.firstName} ${friend.lastName}`));
 
         expect(onClickHandler).toHaveBeenCalledOnce();
