f49f2998a97630c8ce337be26b7157bef47a3dc4
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-16 21:22:54 +0200

    client/test/refactor: extract FriendsList render in a beforeEach

--- client/src/components/pages/user-home-page/friends-section/friends-list/FriendsList.spec.jsx
+++ client/src/components/pages/user-home-page/friends-section/friends-list/FriendsList.spec.jsx
@@ -1,32 +1,28 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it, vi, beforeEach } from "vitest";
 
 import FriendsList from "./FriendsList";
 
 vi.mock("./friend-item/FriendItem", () => ({
     default: ({ friend }) => <div data-testid="friend-item">{friend}</div>
-}))
+}));
+
+const matchingFriends = ["friend1", "friend2"];
+
+beforeEach(() => {
+    render(
+        <FriendsList
+            matchingFriends={matchingFriends}
+        />
+    );
+});
 
 describe("FriendsList component", () => {
-    const matchingFriends = ["friend1", "friend2"];
-
     it("renders the correct number of FriendItem components", () => {
-        render(
-            <FriendsList
-                matchingFriends={matchingFriends}
-            />
-        );
-
         expect(screen.getAllByTestId("friend-item")).toHaveLength(matchingFriends.length);
     });
 
     it("renders FriendItem with passed props", () => {
-        render(
-            <FriendsList
-                matchingFriends={matchingFriends}
-            />
-        );
-
         expect(screen.getAllByTestId("friend-item").at(0)).toHaveTextContent(matchingFriends[0]);
     });
 });
