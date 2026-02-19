e24816ed8ff52a8369462eb71df75b0c49affebb
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-19 22:59:12 +0200

    client/test/refactor: separate PostHeader tests and improve test names

--- client/src/components/shared/post/post-header/PostHeader.spec.jsx
+++ client/src/components/shared/post/post-header/PostHeader.spec.jsx
@@ -7,21 +7,23 @@ import { PostContext } from "../../../../contexts/post-context.js";
 
 import PostHeader from "./PostHeader.jsx";
 
-const post = {
-    owner: {
-        _id: "owner123",
-        imageUrl: "https://example.org/test-123",
-        firstName: "John",
-        lastName: "Doe",
+const postCtxProps = {
+    post: {
+        owner: {
+            _id: "owner123",
+            imageUrl: "https://example.org/test-123",
+            firstName: "John",
+            lastName: "Doe",
+        },
+        _id: "post056",
+        postedOn: "02.05.2025",
     },
-    _id: "post056",
-    postedOn: "02.05.2025",
 };
 
 beforeEach(() => {
     render(
         <MemoryRouter>
-            <PostContext.Provider value={{ post }}>
+            <PostContext.Provider value={{ ...postCtxProps }}>
                 <PostHeader />
             </PostContext.Provider>
         </MemoryRouter>
@@ -29,11 +31,16 @@ beforeEach(() => {
 });
 
 describe("PostHeader component", () => {
-    it("renders with post data", () => {
-        expect(screen.getByRole("img")).toHaveAttribute("src", post.owner.imageUrl);
+    it("renders image with correct src attribute value", () => {
+        expect(screen.getByRole("img")).toHaveAttribute("src", postCtxProps.post.owner.imageUrl);
+    });
 
-        expect(screen.getByRole("link", { name: `${post.owner.firstName} ${post.owner.lastName}` })).toHaveAttribute("href", `/profile/${post.owner._id}`);
+    it("renders posted on link with correct text value and href attribute", () => {
+        expect(screen.getByRole("link", { name: `Posted on ${postCtxProps.post.postedOn}` })).toHaveAttribute("href", `/post/${postCtxProps.post._id}/details`);
+    });
 
-        expect(screen.getByRole("link", { name: `Posted on ${post.postedOn}` })).toHaveAttribute("href", `/post/${post._id}/details`);
+    it("renders username link with correct text value and href attribute", () => {
+        expect(screen.getByRole("link", { name: `${postCtxProps.post.owner.firstName} ${postCtxProps.post.owner.lastName}` })).toHaveAttribute("href", `/profile/${postCtxProps.post.owner._id}`);
+
     });
 });
