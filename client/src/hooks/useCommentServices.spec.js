0af5f2466262d6bcae1f3ab1cfb1e10db801f096
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-22 11:16:30 +0200

    client/test: add parameterized updateComment tests in useCommentServices

--- client/src/hooks/useCommentServices.spec.js
+++ client/src/hooks/useCommentServices.spec.js
@@ -22,12 +22,35 @@ describe("useCommentServices tests", () => {
         { name: "does not create a comment with only spaces as text content", text: " "},
     ])("$name", ({ text }) => {
         const { result } = renderHook(() => useCommentServices());
+        
+        const trimmedText = text.trim();
 
         act(() => {
             result.current.createComment({ text });
         });
 
-        if (text.trim()) {
+        if (trimmedText) {
+            expect(useFetchApiCallMock.fetchExecute).toHaveBeenCalled();
+        } else {
+            expect(useFetchApiCallMock.fetchExecute).not.toHaveBeenCalled();
+        };
+    });
+
+    it.each([
+        { name: "updates a comment with 'Random comment content' as text content", text: "Random comment content"},
+        { name: "does not update a comment with empty text content", text: ""},
+        { name: "does not update a comment with only spaces as text content", text: " "},
+    ])("$name", ({ text }) => {
+        const { result } = renderHook(() => useCommentServices());
+
+        const commentId = "123";
+        const trimmedText = text.trim();
+
+        act(() => {
+            result.current.updateComment( commentId, text );
+        });
+
+        if (trimmedText) {
             expect(useFetchApiCallMock.fetchExecute).toHaveBeenCalled();
         } else {
             expect(useFetchApiCallMock.fetchExecute).not.toHaveBeenCalled();
