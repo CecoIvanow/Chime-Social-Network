56cfb3a4af88cf63d111515d006e743eddffa8ec
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-22 13:08:41 +0200

    client/test: add getUserPosts call test in userUserServices

---
+++ client/src/hooks/useUserServices.spec.js
@@ -0,0 +1,68 @@
+import { act, renderHook } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+
+import useUserServices from "./useUserServices";
+
+vi.mock("./useFetchApiCall.js", () => ({
+    default: () => ({
+        ...useFetchApiCallMock
+    })
+}));
+
+const url = "/users";
+
+const useFetchApiCallMock = {
+    fetchExecute: vi.fn(),
+    isLoading: false,
+    abortFetchRequest: vi.fn(),
+};
+
+describe("useUserServices tests", () => {
+    it("gets all user posts", () => {
+        const { result } = renderHook(() => useUserServices());
+
+        const userId = "123";
+
+        const fullUrl = url + `/${userId}/posts`;
+
+        act(() => {
+            result.current.getUserPosts(userId);
+        });
+
+        expect(useFetchApiCallMock.fetchExecute).toHaveBeenCalledWith(fullUrl);
+    });
+
+    it.skip("aborts all ongoing calls", () => {
+        const { result } = renderHook(() => useUserServices());
+
+        const postId = "123";
+        const text = "Random comment content";
+
+        act(() => {
+            result.current.deletePost(postId);
+            result.current.editPost(postId, text);
+            result.current.createPost({ text });
+            result.current.abortAll();
+        });
+
+        expect(useFetchApiCallMock.abortFetchRequest).toHaveBeenNthCalledWith(
+            1,
+            `${url}/${postId}`,
+            "DELETE"
+        );
+
+        expect(useFetchApiCallMock.abortFetchRequest).toHaveBeenNthCalledWith(
+            2,
+            `${url}/${postId}`,
+            "PATCH"
+        );
+
+        expect(useFetchApiCallMock.abortFetchRequest).toHaveBeenNthCalledWith(
+            3,
+            url,
+            "POST"
+        );
+
+        expect(useFetchApiCallMock.abortFetchRequest).toHaveBeenCalledTimes(3);
+    });
+});
