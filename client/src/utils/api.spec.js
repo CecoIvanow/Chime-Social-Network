3a20efe7e63cb67284333a15ff553533c5476138
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-02-28 21:47:31 +0200

    client/test: add basic PUT fetch test in api.js

--- client/src/utils/api.spec.js
+++ client/src/utils/api.spec.js
@@ -4,24 +4,32 @@ import api from "./api";
 
 window.fetch = vi.fn();
 
-const pathname = '/posts';
+const fetchBody = {
+    post: {
+        _id: "14324",
+        content: "This is a test!",
+    },
+};
+
+const pathname = `/posts/${fetchBody.post._id}`;
 const FULL_PATH = `http://localhost:4012${pathname}`;
 
-const fetchBodyValue = "This is the fetch response!";
-
 function fetchSetup(options = {
-    respBodyPresense: false,
+    responceBody: null,
     method: "GET",
-    fetchOptions: {}
+    options: {},
+    responce: false,
 }) {
     const headersOptions = {};
     let resp;
 
-    if (options.respBodyPresense) {
+    if (options.responceBody) {
+        options.options.body = JSON.stringify(options.responceBody);
+
         headersOptions.headers = {
             "Content-type": "application/json",
         };
-        resp = new Response(JSON.stringify(fetchBodyValue), headersOptions);
+        resp = new Response(JSON.stringify(fetchBody), headersOptions);
     } else {
         headersOptions.headers = {
             "Content-type": "",
@@ -32,22 +40,35 @@ function fetchSetup(options = {
     window.fetch.mockResolvedValue(resp);
 
     const fetchOptionsObj = {
-        ...options.fetchOptions,
+        ...options.options,
         method: options.method,
         headers: {
             'Content-type': 'application/json',
         },
-        credentials: 'include'
-    }
+        credentials: 'include',
+    };
 
-    return fetchOptionsObj
-}
+    return {
+        fetchOptions: fetchOptionsObj,
+    };
+};
 
 describe("API util tests", () => {
-    it("makes a basic GET fetch request with url and no options", async () => {
-        const fetchOptions = fetchSetup();
+    it("makes a basic GET fetch request", async () => {
+        const { fetchOptions } = fetchSetup();
 
         await api.get(pathname);
+        expect(window.fetch).toHaveBeenCalledWith(FULL_PATH, fetchOptions);
+    });
+
+    it("makes a basic PUT fetch request", async () => {
+        const { fetchOptions } = fetchSetup({
+            responceBody: fetchBody,
+            options: {},
+            method: "PUT",
+        });
+
+        await api.put(pathname, fetchBody);
         expect(window.fetch).toHaveBeenCalledWith(FULL_PATH, fetchOptions);
     });
 });
