2e5e67d5b72abda28f900591d8d1b0916853fd42
Cvetelyn <cvetelyn.ivanov@gmail.com>
2026-03-01 15:54:46 +0200

    client/test/fix: add console.error mock to remove console stderr messages

--- client/src/tests/setup.js
+++ client/src/tests/setup.js
@@ -4,4 +4,5 @@ import "@testing-library/jest-dom/vitest"
 beforeEach(() => {
     vi.resetAllMocks();
     vi.clearAllTimers();
-})
+    vi.spyOn(window.console, "error").mockImplementation(() => undefined);
+});
